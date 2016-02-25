d27e9ece4c4e889341f755c628dde5486da23743
Nik Graf <nik@nikgraf.com>
2016-02-24 22:51:01 -0800

    add rudimentary version

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -2,13 +2,54 @@
 
 import React, { Component } from 'react';
 import type ReactElement from 'react';
+import { Editor, EditorState } from 'draft-js';
 
-export default class Editor extends Component {
+const styles = {
+  root: {
+    fontFamily: '\'Helvetica\', sans-serif',
+    padding: 20,
+    width: 600,
+  },
+  editor: {
+    border: '1px solid #ccc',
+    cursor: 'text',
+    minHeight: 80,
+    padding: 10,
+  },
+  button: {
+    marginTop: 10,
+    textAlign: 'center',
+  },
+};
+
+export default class UnicornEditor extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {editorState: EditorState.createEmpty()};
+
+    this.focus = () => this.refs.editor.focus();
+    this.onChange = (editorState) => this.setState({editorState});
+    this.logState = () => console.log(this.state.editorState.toJS());
+  }
 
   render(): ReactElement {
     return (
-      <div>
-        I'm the Unicorn Editor.
+      <div style={styles.root}>
+        <div style={styles.editor} onClick={this.focus}>
+          <Editor
+            editorState={this.state.editorState}
+            onChange={this.onChange}
+            placeholder="I'm a Unicorn!"
+            ref="editor"
+          />
+        </div>
+        <input
+          onClick={this.logState}
+          style={styles.button}
+          type="button"
+          value="Log State"
+        />
       </div>
     );
   }
