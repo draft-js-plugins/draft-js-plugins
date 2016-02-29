1f7f68203d2e1a3123be9f3ceab8b83b292d0fc9
Nik Graf <nik@nikgraf.com>
2016-02-29 00:22:55 -0800

    fix remove sticker behaviour

--- src/components/Editor/modifiers/removeSticker.js
+++ src/components/Editor/modifiers/removeSticker.js
@@ -8,12 +8,10 @@ import {
 
 export default (editorState: Object, blockKey: String): Object => {
   const content = editorState.getCurrentContent();
-  const beforeKey = content.getKeyBefore(blockKey);
-  const beforeBlock = content.getBlockBefore(blockKey);
   const afterKey = content.getKeyAfter(blockKey);
 
   let withoutSticker;
-  if (beforeKey === undefined && afterKey !== undefined) {
+  if (afterKey !== undefined) {
     const targetRange = new SelectionState({
       anchorKey: blockKey,
       anchorOffset: 0,
@@ -26,14 +24,6 @@ export default (editorState: Object, blockKey: String): Object => {
       withoutSticker.getSelectionAfter(),
       'unstyled'
     );
-  } else if (beforeKey !== undefined) {
-    const targetRange = new SelectionState({
-      anchorKey: beforeKey,
-      anchorOffset: beforeBlock.getLength(),
-      focusKey: blockKey,
-      focusOffset: 0,
-    });
-    withoutSticker = Modifier.removeRange(content, targetRange, 'backward');
   } else {
     withoutSticker = Modifier.setBlockType(
       content,
@@ -41,7 +31,6 @@ export default (editorState: Object, blockKey: String): Object => {
       'unstyled'
     );
   }
-
   const newState = EditorState.push(editorState, withoutSticker, 'remove-sticker');
   return EditorState.forceSelection(newState, withoutSticker.getSelectionAfter());
 };
