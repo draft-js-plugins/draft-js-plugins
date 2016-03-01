b64dd38bfcd5899e5cf70a1dc52ce983030fca2d
Nik Graf <nik@nikgraf.com>
2016-02-29 21:58:42 -0800

    add headline

--- site/index.js
+++ site/index.js
@@ -5,4 +5,9 @@ import UnicornEditor from './components/UnicornEditor';
 // export for http://fb.me/react-devtools
 window.React = React;
 
-ReactDOM.render(<div>Why? Because Unicorns are cooler than Cats! <UnicornEditor /></div>, document.getElementById('react'));
+ReactDOM.render(
+  <div>
+    <h1>Why? Because Unicorns are cooler than Cats!</h1>
+    <UnicornEditor />
+  </div>, document.getElementById('react')
+);
