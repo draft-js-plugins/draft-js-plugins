c0c586bc3997b5d8a4a348cd0f2a0e5e2ea5158a
jpuri <jyotipuri@gmail.com>
2016-03-06 15:56:23 +0530

    feat(Mentions): adding hover state and filtering for mentions

--- src/mentionPlugin/MentionSearch/MentionOption/styles.js
+++ src/mentionPlugin/MentionSearch/MentionOption/styles.js
@@ -5,4 +5,12 @@ export default {
     borderBottom: '1px solid #ddd',
     padding: 5,
   },
+  hovered: {
+    // I am copying root styles also to hovered so that during
+    // render we do not need to merge the objects to create anew object each time.
+    borderBottom: '1px solid #ddd',
+    padding: 5,
+    backgroundColor: '#000080',
+    color: 'white',
+  },
 };
--- src/mentionPlugin/MentionSearch/index.js
+++ src/mentionPlugin/MentionSearch/index.js
@@ -14,12 +14,16 @@ export default (mentions) => {
     };
 
     // Get the first 5 mentions that match
-    getMentionsForFilter = () => (
-      mentions
+    getMentionsForFilter = () => {
 
-      // TODO better search algorithm than startsWith
-      // mentions.filter((mention) => mention.get('handle').startsWith(this.mentionSearch)).slice(0, 10)
-    );
+      const userEntered = this.props.editor.props.editorState.getCurrentContent().getLastBlock().getText();
+      const lastMentionStarts = userEntered.lastIndexOf("@");
+      const mentionValue = userEntered.substring(lastMentionStarts + 1, userEntered.length);
+      let filteredValues = mentions && mentions.filter((mention) =>
+        !mentionValue || mention.get('handle').toLowerCase().indexOf(mentionValue.toLowerCase()) > -1);
+      const size = filteredValues.size < 5 ? filteredValues.size : 5;
+      return filteredValues.setSize(size);
+    };
 
     renderItemForMention = (mention) => (
 
@@ -34,10 +38,12 @@ export default (mentions) => {
     render() {
       // TODO ask issac to provide begin & end down to the component as prop (in decorators)
       this.lastSelection = this.props.editor.props.editorState.getSelection();
+      const filteredMentions = this.getMentionsForFilter();
       return (
         <span {...this.props} style={ styles.root }>
           { this.props.children }
-          <Dropdown>
+          { filteredMentions.size > 0 ?
+            <Dropdown>
             {
               this.getMentionsForFilter().map((mention) => (
                 <MentionOption
@@ -48,6 +54,7 @@ export default (mentions) => {
               ))
             }
           </Dropdown>
+          : void 0}
         </span>
       );
     }
