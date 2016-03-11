d40fdb0ef0017cf40838c6d8caa0b5b5f98f0db7
Nik Graf <nik@nikgraf.com>
2016-03-10 22:28:17 -0800

    fix StatePreview

--- site/components/StatePreview/index.js
+++ site/components/StatePreview/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './styles';
 import { shouldComponentUpdate } from 'react-addons-pure-render-mixin';
 
-export default class StatePreview extends PureComponent {
+export default class StatePreview extends Component {
 
   shouldComponentUpdate = shouldComponentUpdate;
 
