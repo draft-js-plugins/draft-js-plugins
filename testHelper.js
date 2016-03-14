400c4eb0fd475342ce8c361045e15231945cf10a
Nik Graf <nik@nikgraf.com>
2016-03-13 20:18:03 -0700

    fix tests by mocking css modules

--- testHelper.js
+++ testHelper.js
@@ -1,8 +1,13 @@
 import chai from 'chai';
 import dirtyChai from 'dirty-chai';
 import chaiEnzyme from 'chai-enzyme';
+import hook from 'css-modules-require-hook';
 
 process.env.NODE_ENV = 'test';
+
+hook({
+  generateScopedName: '[name]__[local]___[hash:base64:5]',
+});
 
 chai.use(dirtyChai);
 chai.use(chaiEnzyme());
