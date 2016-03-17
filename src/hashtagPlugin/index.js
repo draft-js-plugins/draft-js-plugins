66522246ee4f701b6be3d0ed283611354e83a6db
Nik Graf <nik@nikgraf.com>
2016-03-17 11:33:46 -0700

    new strategy for theming hashtags

--- src/hashtagPlugin/index.js
+++ src/hashtagPlugin/index.js
@@ -1,23 +1,33 @@
 import Hashtag from './Hashtag';
 import hashtagStrategy from './hashtagStrategy';
 import { fromJS } from 'immutable';
-import styles from './Hashtag/styles.css';
+import styles from './styles.css';
+import decorateComponentWithProps from '../utils/decorateComponentWithProps';
 
-const defaultHashTagTheme = fromJS({
-  hashtag: styles.root,
+const defaultTheme = fromJS({
+  hashtag: styles.hashtag,
 });
 
-const hashtagPlugin = (config = {}) => ({
-  pluginProps: {
-    decorators: [
-      {
-        strategy: hashtagStrategy,
-        component: Hashtag,
-        theme: defaultHashTagTheme.merge(config.theme),
-      },
-    ],
-  },
-  Hashtag,
-});
+const hashtagPlugin = (config = {}) => {
+  // Styles are overwritten instead of merged as merging causes a lot of confusion.
+  //
+  // Why? Because when merging a developer needs to know all of the underlying
+  // styles which needs a deep dive into the code. Merging also makes it prone to
+  // errors when upgrading as basically every styling change would become a major
+  // breaking change. 1px of an increased padding can break a whole layout.
+  const theme = config.theme ? config.theme : defaultTheme;
+  const DecoratedHashtag = decorateComponentWithProps(Hashtag, { theme });
+  return {
+    pluginProps: {
+      decorators: [
+        {
+          strategy: hashtagStrategy,
+          component: DecoratedHashtag,
+        },
+      ],
+    },
+    Hashtag: DecoratedHashtag, // TODO not sure if there is a value in exporting this here
+  };
+};
 
 export default hashtagPlugin;
