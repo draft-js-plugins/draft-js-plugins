fc7f0eaf51cd05bda74087c81f2f90e29bbecf3e
Nik Graf <nik@nikgraf.com>
2016-03-16 23:53:06 -0700

    temporarily remove navigation

--- docs/client/components/wrappers/Page/index.js
+++ docs/client/components/wrappers/Page/index.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import { Link, IndexLink } from 'react-router';
+
+// import { Link, IndexLink } from 'react-router';
+//
+// <nav>
+//   <IndexLink to="/" activeClassName={'wow'}>Home</IndexLink>
+//   <Link to="/about" activeClassName={'that'}>About</Link>
+// </nav>
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <nav>
-          <IndexLink to="/" activeClassName={'wow'}>Home</IndexLink>
-          <Link to="/about" activeClassName={'that'}>About</Link>
-        </nav>
         {this.props.children}
       </div>
     );
