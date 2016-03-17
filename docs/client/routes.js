a47f77343240401cbf83357dfc5b3a2730177b22
Nik Graf <nik@nikgraf.com>
2016-03-16 17:49:53 -0700

    fix routes

--- docs/client/routes.js
+++ docs/client/routes.js
@@ -5,11 +5,14 @@ import App from './components/App';
 import NotFound from './components/NotFound';
 import Home from './components/Home';
 import About from './components/About';
+import Wrapper from './components/Wrapper';
 
 export const routes = (
-  <Route path="/" title="App" component={App}>
+  <Route path="/" title="App" component={Wrapper}>
     <IndexRoute component={Home} />
-    <Route path="about" title="App - About" component={About} />
+    <Route path="/" title="App" component={App}>
+      <Route path="about" title="App - About" component={About} />
+    </Route>
     <Route path="*" title="404: Not Found" component={NotFound} />
   </Route>
 );
