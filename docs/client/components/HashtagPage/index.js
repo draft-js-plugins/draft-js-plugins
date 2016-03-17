22169b98dda26ec132f604cee0f614f27c832a33
Nik Graf <nik@nikgraf.com>
2016-03-16 22:04:24 -0700

    fix linting issues

--- docs/client/components/HashtagPage/index.js
+++ docs/client/components/HashtagPage/index.js
@@ -19,7 +19,7 @@ export default class App extends Component {
 
   render() {
     return (
-      <div>
+      <div className={ styles.root }>
         Test
       </div>
     );
--- docs/client/components/UndoPage/index.js
+++ docs/client/components/UndoPage/index.js
@@ -19,7 +19,7 @@ export default class App extends Component {
 
   render() {
     return (
-      <div>
+      <div className={ styles.root }>
         Undo details
       </div>
     );
