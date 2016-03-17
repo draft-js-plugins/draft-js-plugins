af098e1beb0a4edd2aa44dd5747d0391dcc1ffee
Nik Graf <nik@nikgraf.com>
2016-03-16 17:22:37 -0700

    move site to docs

--- site/index.js
+++
@@ -1,9 +0,0 @@
-import 'babel-polyfill';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-
-// export for http://fb.me/react-devtools
-window.React = React;
-
-ReactDOM.render(<App />, document.getElementById('react'));
