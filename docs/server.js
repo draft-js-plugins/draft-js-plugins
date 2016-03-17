1f472407af09cfffd46d9ef7fdfc3265db4038ae
Nik Graf <nik@nikgraf.com>
2016-03-16 17:15:42 -0700

    add base styles

--- docs/server.js
+++ docs/server.js
@@ -11,18 +11,34 @@ import webpack from 'webpack';
 import config from './webpack.config.dev.js';
 
 const Html = ({
-  title = 'Rainbow Unicorns',
+  title = 'DraftJS Plugins (including the ultimate Unicorn Editor)',
   bundle = '/app.js',
   body = '',
   stylesheet = '',
+
+  // googleAnalytics = `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  // (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  // m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  // })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  // ga('create', 'UA-75176147-1', 'auto');
+  // ga('send', 'pageview');`,
+
+  // <script>
+  //   { googleAnalytics }
+  // </script>
+
 }) => (
   <html lang="en">
     <head>
       <meta charSet="utf-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
       <title>{title}</title>
-      {stylesheet && <link rel="stylesheet" href={stylesheet} />}
+      <link rel="stylesheet" href="css/normalize.css" />
+      <link rel="stylesheet" href="css/base.css" />
+      <link rel="stylesheet" href="css/Draft.css" />
+      { stylesheet && <link rel="stylesheet" href={stylesheet} /> }
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,300" rel="stylesheet" type="text/css" />
     </head>
     <body>
       <div id="root" dangerouslySetInnerHTML={{ __html: body }} />
