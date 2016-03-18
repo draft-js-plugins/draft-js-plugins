2e2369c9e94f9ea38cba147aed943f9ad5f0a12c
Nik Graf <nik@nikgraf.com>
2016-03-17 21:34:17 -0700

    add prism styles for the highlighted code

--- docs/client/components/shared/Code/index.js
+++ docs/client/components/shared/Code/index.js
@@ -1,28 +1,20 @@
-import {
-  default as React,
-  Component,
-  PropTypes,
-} from 'react';
+import React, { Component, PropTypes } from 'react';
 import { shouldComponentUpdate } from 'react-addons-pure-render-mixin';
 import styles from './styles.css';
-
-// require('prismjs/themes/prism.css');
+import '!style!css!prismjs/themes/prism.css';
 
 export default class Code extends Component {
 
   static propTypes = {
     code: PropTypes.string,
-    children: PropTypes.any,
   };
 
   shouldComponentUpdate = shouldComponentUpdate;
 
   render() {
-    const codeClassName = (this.props.language ? `language-${this.props.language}` : '');
     return (
       <pre className={ styles.root } >
         <code
-          className={ codeClassName }
           dangerouslySetInnerHTML={{ __html: this.props.code }}
         />
       </pre>
