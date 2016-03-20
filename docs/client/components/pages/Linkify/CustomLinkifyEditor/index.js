87e299ecec00bd8d46d4a03074370a40f5441fbb
jpuri <jyotipuri@gmail.com>
2016-03-20 19:32:34 +0530

    docs(Sticker, Linkify, Mention): adding editors to docs pages

---
+++ docs/client/components/pages/Linkify/CustomLinkifyEditor/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { Map } from 'immutable';
+import { EditorState } from 'draft-js';
+import Editor from 'draft-js-plugin-editor';
+import linkifyPlugin from 'draft-js-linkify-plugin';
+import styles from './styles.css';
+import { fromJS } from 'immutable';
+
+const linkifyPluginInstance = linkifyPlugin();
+const plugins = [linkifyPluginInstance];
+
+export default class CustomMentionEditor extends Component {
+
+  state = {
+    editorState: EditorState.createEmpty(),
+  };
+
+  onChange = (editorState) => {
+    this.setState({
+      editorState,
+    });
+  };
+
+  focus = () => {
+    this.refs.editor.focus();
+  };
+
+  render() {
+    return (
+      <div className={ styles.root }>
+        <div className={ styles.editor } onClick={ this.focus }>
+          <Editor
+            editorState={this.state.editorState}
+            onChange={this.onChange}
+            plugins={plugins}
+            ref="editor"
+          />
+        </div>
+      </div>
+    );
+  }
+}
--- docs/client/components/pages/Linkify/index.js
+++ docs/client/components/pages/Linkify/index.js
@@ -1,33 +1,16 @@
-/* eslint-disable react/no-unknown-property */
 import React, { Component } from 'react';
 import Container from '../../shared/Container';
 import AlternateContainer from '../../shared/AlternateContainer';
 import Heading from '../../shared/Heading';
-import SimpleEditor from '../../shared/SimpleEditor';
 import styles from './styles.css';
-import linkifyPlugin from 'draft-js-linkify-plugin';
-import { Map } from 'immutable';
-
-const pluginMap = new Map({
-  linkify: linkifyPlugin(),
-});
-const Editor = SimpleEditor(pluginMap);
+import Code from '../../shared/Code';
+import SimpleLinkifyEditor from './SimpleLinkifyEditor';
+import CustomLinkifyEditor from './CustomLinkifyEditor';
+import simpleExampleCode from '!!../../../loaders/prism-loader?language=javascript!./SimpleLinkifyEditor';
+import customExampleCode from '!!../../../loaders/prism-loader?language=javascript!./CustomLinkifyEditor';
+import customExampleStylesCode from '!!../../../loaders/prism-loader?language=css!./CustomLinkifyEditor/styles.css';
 
 export default class App extends Component {
-
-  // description what the plugin does
-
-  // h2 "Simple Example"
-  // an simple editor with hashtag plugin (can be copied from unicorn editor)
-  // simple code example
-
-  // h2 "Custom Styling"
-  // an editor with hashtag plugin & custom styles (can be copied from unicorn editor)
-  // custom styles code example
-
-  // h2 "API"
-  // a table explaining what is exported
-
   render() {
     return (
       <div>
@@ -36,8 +19,15 @@ export default class App extends Component {
           <div className={ styles.root }>Linkify</div>
         </AlternateContainer>
         <Container>
-          <Heading level={ 2 }>Example</Heading>
-          <Editor />
+          <Heading level={ 2 }>Simple Example</Heading>
+          <SimpleLinkifyEditor />
+          <Code code={ simpleExampleCode } name="SimpleLinkifyEditor.js" />
+        </Container>
+        <Container>
+          <Heading level={ 2 }>Themed Linkify Example</Heading>
+          <CustomLinkifyEditor />
+          <Code code={ customExampleCode } name="CustomLinkifyEditor.js" />
+          <Code code={ customExampleStylesCode } name="styles.css" />
         </Container>
       </div>
 
