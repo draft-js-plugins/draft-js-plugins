676b5ce6c9efc9f47fbbbdf4c8ad6f82fd4dd9c7
Nik Graf <nik@nikgraf.com>
2016-03-20 00:31:51 -0700

    fix test suite

--- src/mentionPlugin/Mention/__test__/index.js
+++ src/mentionPlugin/Mention/__test__/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, shallow } from 'enzyme';
 import Mention from '../index';
-import { fromJS } from 'immutable';
+import { fromJS, Map } from 'immutable';
 import { Entity } from 'draft-js';
 import { expect } from 'chai';
 
@@ -11,14 +11,14 @@ describe('Mention', () => {
       link: 'https://www.example.com/john',
     });
     const entityKey = Entity.create('mention', 'SEGMENTED', { mention });
-    const result = shallow(<Mention entityKey={ entityKey } />);
+    const result = shallow(<Mention entityKey={ entityKey } theme={ Map() } />);
     expect(result).to.have.tagName('a');
   });
 
   it('renders a Span tag in case no link is provided', () => {
     const mention = fromJS({});
     const entityKey = Entity.create('mention', 'SEGMENTED', { mention });
-    const result = render(<Mention entityKey={ entityKey } />);
+    const result = render(<Mention entityKey={ entityKey } theme={ Map() } />);
     expect(result).to.have.tagName('span');
   });
 });
