c156e62c4894dab3f4b78c9652dcbcd85bcab0bf
Nik Graf <nik@nikgraf.com>
2016-03-19 23:49:54 -0700

    update script to work with new docs

--- scripts/publish_gh_pages.sh
+++ scripts/publish_gh_pages.sh
@@ -2,15 +2,15 @@ git checkout master
 git checkout -b tmp-gh-pages
 rm .gitignore
 
-cd site
+cd docs
 npm prune
 npm install
 npm run build
 
-git add static/bundle.js
+git add public
 git commit -am 'add files'
 cd ..
-git subtree split --prefix site -b gh-pages
+git subtree split --prefix docs/public -b gh-pages
 git push -f origin gh-pages:gh-pages
 git checkout master
 git branch -D tmp-gh-pages
