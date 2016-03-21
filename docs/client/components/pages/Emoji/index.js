4cd361ff0f8fa843db5fe69a92644eb5f92ac9dd
jpuri <jyotipuri@gmail.com>
2016-03-21 12:23:20 +0530

    docs(Emoji): adding more details to the page

--- docs/client/components/pages/Emoji/index.js
+++ docs/client/components/pages/Emoji/index.js
@@ -1,35 +1,55 @@
-/* eslint-disable react/no-unknown-property */
 import React, { Component } from 'react';
-import styles from './styles.css';
 import Container from '../../shared/Container';
 import AlternateContainer from '../../shared/AlternateContainer';
 import Heading from '../../shared/Heading';
+import styles from './styles.css';
+import Code from '../../shared/Code';
+import SimpleEmojiEditor from './SimpleEmojiEditor';
+import CustomEmojiEditor from './CustomEmojiEditor';
+import simpleExampleCode from '!!../../../loaders/prism-loader?language=javascript!./SimpleEmojiEditor';
+import customExampleCode from '!!../../../loaders/prism-loader?language=javascript!./CustomEmojiEditor';
+import customExampleStylesCode from '!!../../../loaders/prism-loader?language=css!./CustomEmojiEditor/styles.css';
+import gettingStarted from '!!../../../loaders/prism-loader?language=javascript!./gettingStarted';
 
 export default class App extends Component {
-
-  // description what the plugin does
-
-  // h2 "Simple Example"
-  // an simple editor with hashtag plugin (can be copied from unicorn editor)
-  // simple code example
-
-  // h2 "Custom Styling"
-  // an editor with hashtag plugin & custom styles (can be copied from unicorn editor)
-  // custom styles code example
-
-  // h2 "API"
-  // a table explaining what is exported
-
   render() {
     return (
       <div>
         <Container>
+          <Heading level={ 2 }>Emoji</Heading>
           <div className={ styles.root }>Emoji</div>
         </Container>
         <AlternateContainer>
           <Heading level={ 2 }>Getting Started</Heading>
+          <Code code="npm install draft-js-emoji-plugin --save" />
+          <Code code={ gettingStarted } name="gettingStarted.js" />
         </AlternateContainer>
+        <Container>
+          <Heading level={ 2 }>Configuration Parameters</Heading>
+          <div>
+            <div className={ styles.paramName }>theme</div>
+            <span>map of CSS classes to style the plugin</span>
+            <table className={ styles.themeTable }>
+              <tr>
+                <td className={ styles.themeProperty }>Emoji</td>
+                <td>CSS class to be applied to emoji</td>
+              </tr>
+            </table>
+          </div>
+        </Container>
+        <Container>
+          <Heading level={ 2 }>Simple Example</Heading>
+          <SimpleEmojiEditor />
+          <Code code={ simpleExampleCode } name="SimpleEmojiEditor.js" />
+        </Container>
+        <Container>
+          <Heading level={ 2 }>Themed Emoji Example</Heading>
+          <CustomEmojiEditor />
+          <Code code={ customExampleCode } name="CustomEmojiEditor.js" />
+          <Code code={ customExampleStylesCode } name="styles.css" />
+        </Container>
       </div>
+
     );
   }
 }
