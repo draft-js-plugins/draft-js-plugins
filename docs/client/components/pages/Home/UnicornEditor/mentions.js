f982e9af7a349775123e54838cb08d3e75f5cdd9
Pascal Brandt <psbrandt@gmail.com>
2016-03-21 23:06:37 -0700

    👨 Fixed avatar and extended mention list.

--- docs/client/components/pages/Home/UnicornEditor/mentions.js
+++ docs/client/components/pages/Home/UnicornEditor/mentions.js
@@ -13,6 +13,11 @@ const mentions = fromJS([
     avatar: 'https://pbs.twimg.com/profile_images/517863945/mattsailing_400x400.jpg',
   },
   {
+    name: 'Julian Krispel-Samsel',
+    link: 'https://twitter.com/juliandoesstuff',
+    avatar: 'https://pbs.twimg.com/profile_images/477132877763579904/m5bFc8LF_400x400.png',
+  },
+  {
     name: 'Jyoti Puri',
     link: 'https://twitter.com/jyopur',
     avatar: 'https://pbs.twimg.com/profile_images/705714058939359233/IaJoIa78_400x400.jpg',
@@ -26,6 +31,11 @@ const mentions = fromJS([
     name: 'Nik Graf',
     link: 'https://twitter.com/nikgraf',
     avatar: 'https://pbs.twimg.com/profile_images/535634005769457664/Ppl32NaN_400x400.jpeg',
+  },
+  {
+    name: 'Pascal Brandt',
+    link: 'https://twitter.com/psbrandt',
+    avatar: 'https://pbs.twimg.com/profile_images/688487813025640448/E6O6I011_400x400.png',
   },
 ]);
 
