8cc5c8562f8b0979d8391fa00a6ff68bc8039d91
Nik Graf <nik@nikgraf.com>
2016-03-25 12:50:11 -0700

    improve build for hashtags

--- draft-js-hashtag-plugin/webpack.config.js
+++ draft-js-hashtag-plugin/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     publicPath: '/',
     libraryTarget: 'commonjs2', // necessary for the babel plugin
-    path: path.join(__dirname, 'lib'), // where to place webpack files
+    path: path.join(__dirname, 'lib-css'), // where to place webpack files
   },
   module: {
     loaders: [
@@ -19,6 +19,6 @@ module.exports = {
   },
   postcss: [autoprefixer({ browsers: ['> 1%'] })],
   plugins: [
-    new ExtractTextPlugin('plugin.css', { allChunks: true }),
+    new ExtractTextPlugin(`${path.parse(process.argv[2]).name}.css`),
   ],
 };
