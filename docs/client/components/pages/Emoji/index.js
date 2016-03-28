25cc3c1d490fb76841e9297355aeeb4d098c7251
Nik Graf <nik@nikgraf.com>
2016-03-27 17:05:25 -0700

    add default style instructions

--- docs/client/components/pages/Emoji/index.js
+++ docs/client/components/pages/Emoji/index.js
@@ -14,6 +14,10 @@ import gettingStarted from '!!../../../loaders/prism-loader?language=javascript!
 
 import simpleExampleCode from '!!../../../loaders/prism-loader?language=javascript!./SimpleEmojiEditor';
 import simpleEditorStylesCode from '!!../../../loaders/prism-loader?language=css!./SimpleEmojiEditor/editorStyles.css';
+import webpackConfig from '!!../../../loaders/prism-loader?language=javascript!./webpackConfig';
+import webpackImport from '!!../../../loaders/prism-loader?language=javascript!./webpackImport';
+import ExternalLink from '../../shared/Link';
+import InlineCode from '../../shared/InlineCode';
 
 export default class App extends Component {
   render() {
@@ -41,6 +45,35 @@ export default class App extends Component {
           <Heading level={ 2 }>Getting Started</Heading>
           <Code code="npm install draft-js-emoji-plugin --save" />
           <Code code={ gettingStarted } name="gettingStarted.js" />
+          <Heading level={ 3 }>Importing the default styles</Heading>
+          <p>
+            The plugin ships with a default styling available at this location in the installed package:
+            &nbsp;
+            <InlineCode code={ 'node_modules/draft-js-hashtag-plugin/lib/plugin.css' } />
+          </p>
+          <Heading level={ 4 }>Webpack Usage</Heading>
+          <ul className={ styles.list }>
+            <li className={ styles.listEntry }>
+              1. Install Webpack loaders:
+              &nbsp;
+              <InlineCode code={ 'npm i style-loader css-loader --save-dev' } />
+            </li>
+            <li className={ styles.listEntry }>
+              2. Add the below section to Webpack config (if your config already has a loaders array, simply add the below loader object to your existing list.
+              <Code code={ webpackConfig } className={ styles.guideCodeBlock } />
+            </li>
+            <li className={ styles.listEntry }>
+              3. Add the below import line to your component to tell Webpack to inject the style to your component.
+              <Code code={ webpackImport } className={ styles.guideCodeBlock } />
+            </li>
+            <li className={ styles.listEntry }>
+              4. Restart Webpack.
+            </li>
+          </ul>
+          <Heading level={ 4 }>Browserify Usage</Heading>
+          <p>
+            Please help, by submiting a Pull Request to the <ExternalLink href="https://github.com/draft-js-plugins/draft-js-plugins/blob/master/docs/client/components/pages/Hashtag/index.js">documention</ExternalLink>.
+          </p>
         </AlternateContainer>
         <Container>
           <Heading level={ 2 }>Configuration Parameters</Heading>
--- docs/client/components/pages/Mention/index.js
+++ docs/client/components/pages/Mention/index.js
@@ -16,6 +16,10 @@ import gettingStarted from '!!../../../loaders/prism-loader?language=javascript!
 import SocialBar from '../../shared/SocialBar';
 import NavBar from '../../shared/NavBar';
 import Separator from '../../shared/Separator';
+import webpackConfig from '!!../../../loaders/prism-loader?language=javascript!./webpackConfig';
+import webpackImport from '!!../../../loaders/prism-loader?language=javascript!./webpackImport';
+import ExternalLink from '../../shared/Link';
+import InlineCode from '../../shared/InlineCode';
 
 export default class App extends Component {
   render() {
@@ -30,6 +34,35 @@ export default class App extends Component {
           <Heading level={ 2 }>Getting Started</Heading>
           <Code code="npm install draft-js-mention-plugin --save" />
           <Code code={ gettingStarted } name="gettingStarted.js" />
+          <Heading level={ 3 }>Importing the default styles</Heading>
+          <p>
+            The plugin ships with a default styling available at this location in the installed package:
+            &nbsp;
+            <InlineCode code={ 'node_modules/draft-js-hashtag-plugin/lib/plugin.css' } />
+          </p>
+          <Heading level={ 4 }>Webpack Usage</Heading>
+          <ul className={ styles.list }>
+            <li className={ styles.listEntry }>
+              1. Install Webpack loaders:
+              &nbsp;
+              <InlineCode code={ 'npm i style-loader css-loader --save-dev' } />
+            </li>
+            <li className={ styles.listEntry }>
+              2. Add the below section to Webpack config (if your config already has a loaders array, simply add the below loader object to your existing list.
+              <Code code={ webpackConfig } className={ styles.guideCodeBlock } />
+            </li>
+            <li className={ styles.listEntry }>
+              3. Add the below import line to your component to tell Webpack to inject the style to your component.
+              <Code code={ webpackImport } className={ styles.guideCodeBlock } />
+            </li>
+            <li className={ styles.listEntry }>
+              4. Restart Webpack.
+            </li>
+          </ul>
+          <Heading level={ 4 }>Browserify Usage</Heading>
+          <p>
+            Please help, by submiting a Pull Request to the <ExternalLink href="https://github.com/draft-js-plugins/draft-js-plugins/blob/master/docs/client/components/pages/Hashtag/index.js">documention</ExternalLink>.
+          </p>
         </AlternateContainer>
         <Container>
           <Heading level={ 2 }>Configuration Parameters</Heading>
