8f6c55a008fe7ce13bc3ba2bf6793e50a064b80d
Nik Graf <nik@nikgraf.com>
2016-03-29 01:07:26 -0700

    fix emoji search

--- draft-js-emoji-plugin/src/EmojiSearch/index.js
+++ draft-js-emoji-plugin/src/EmojiSearch/index.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import EmojiOption from './EmojiOption';
 import addEmoji from '../modifiers/addEmoji';
 import getSearchText from '../utils/getSearchText';
+import decodeOffsetKey from '../utils/decodeOffsetKey';
 import { genKey } from 'draft-js';
 import emojiShortNames from '../utils/shortNames';
 
@@ -10,7 +11,7 @@ export default class EmojiSearch extends Component {
 
   state = {
     focusedOptionIndex: 0,
-    isOpen: true,
+    isOpen: false,
   };
 
   componentWillMount() {
@@ -20,27 +21,13 @@ export default class EmojiSearch extends Component {
     // It assumes that the keyFunctions object will not loose its reference and
     // by this we can replace inner parameters spread over different modules.
     // This better be some registering & unregistering logic. PRs are welcome :)
-    this.props.callbacks.onDownArrow = this.onDownArrow;
-    this.props.callbacks.onUpArrow = this.onUpArrow;
-    this.props.callbacks.onEscape = this.onEscape;
-    this.props.callbacks.onTab = this.onTab;
-    this.props.callbacks.handleReturn = this.commitSelection;
-    this.props.callbacks.onChange = this.onEditorStateChange;
-    this.props.ariaProps.ariaActiveDescendantID = `emoji-option-${this.key}-${this.state.focusedOptionIndex}`;
-    this.props.ariaProps.ariaOwneeID = `emojis-list-${this.key}`;
+    this.props.callbacks.onChange = this.props.callbacks.onChange.set(this.key, this.onEditorStateChange);
   }
 
   componentDidMount() {
-    // This a really nasty way of attaching & releasing the key related functions.
-    // It assumes that the ariaProps object will not loose its reference and
-    // by this we can replace inner parameters spread over different modules.
-    // This better be some registering & unregistering logic. PRs are welcome :)
-    if (this.state.isOpen) {
-      this.props.ariaProps.ariaHasPopup = 'true';
-      this.props.ariaProps.ariaExpanded = 'true';
-    } else {
-      this.updateAriaCloseDropdown();
-    }
+    // since the initial state is false we have to set the proper aria states
+    // for a closed popover
+    this.updateAriaCloseDropdown();
 
     // Note: to force a re-render of the outer component to change the aria props
     this.props.updateEditorState(this.props.getEditorState());
@@ -57,61 +44,53 @@ export default class EmojiSearch extends Component {
     }
   };
 
-  componentWillUnmount() {
-    // make sure none of these callbacks are triggered
-    this.props.callbacks.onDownArrow = undefined;
-    this.props.callbacks.onUpArrow = undefined;
-    this.props.callbacks.onEscape = undefined;
-    this.props.callbacks.handleReturn = undefined;
-    this.props.callbacks.onChange = undefined;
-  }
+  componentWillUnmount = () => {
+    this.props.callbacks.onChange = this.props.callbacks.onChange.delete(this.key);
+  };
 
   onEditorStateChange = (editorState) => {
-    // store the initialSelection (must be at the @)
-    if (this.initialSelection === undefined) {
-      this.initialSelection = editorState.getSelection();
-    }
-
     const removeList = () => {
       if (this.state.isOpen) {
-        this.setState({
-          isOpen: false,
-        });
+        this.closeDropdown();
       }
 
       return editorState;
     };
 
+    // identify the start & end positon of the search-text
+    const { blockKey, decoratorKey, leafKey } = decodeOffsetKey(this.props.offsetKey);
+    const { start, end } = editorState
+      .getBlockTree(blockKey)
+      .getIn([decoratorKey, 'leaves', leafKey]);
+
+    // get the current selection
     const selection = editorState.getSelection();
 
     // the list should not be visible in case a range is selected or the editor has no focus
     if (!selection.isCollapsed() || !selection.getHasFocus()) return removeList();
 
     // only show the search component for the current block
-    const sameBlock = selection.getAnchorKey() === this.initialSelection.getAnchorKey();
+    const sameBlock = selection.getAnchorKey() === blockKey;
     if (!sameBlock) return removeList();
 
     // Checks that the curosr is after the @ character but still somewhere in
     // the word (search term). Setting it to allow the cursor being left of
     // the @ causes causes troubles as due selection connfusion.
-    const { begin, end } = getSearchText(editorState, this.initialSelection);
     const anchorOffset = selection.getAnchorOffset();
-    if (anchorOffset <= begin || end < anchorOffset) return removeList();
+    if (anchorOffset <= start || end < anchorOffset) return removeList();
 
     // If none of the above triggered to close the window, it's save to assume
     // the dropdown should be open. This is useful when a user focuses on another
     // input field and then comes back: the dropwdown will again.
     if (!this.state.isOpen) {
-      this.setState({
-        isOpen: true,
-      });
+      this.openDropdown();
     }
 
     return editorState;
   };
 
   onEmojiSelect = (emoji) => {
-    this.updateAriaCloseDropdown();
+    this.closeDropdown();
     const newEditorState = addEmoji(this.props.getEditorState(), emoji);
     this.props.updateEditorState(newEditorState);
   };
@@ -138,17 +117,15 @@ export default class EmojiSearch extends Component {
   onEscape = (keyboardEvent) => {
     keyboardEvent.preventDefault();
 
-    this.updateAriaCloseDropdown();
-    this.setState({
-      isOpen: false,
-    });
+    this.closeDropdown();
 
     // to force a re-render of the outer component to change the aria props
     this.props.updateEditorState(this.props.getEditorState());
   };
 
   onEmojiFocus = (index) => {
-    this.props.ariaProps.ariaActiveDescendantID = `emoji-option-${this.key}-${index}`;
+    const descendant = `emoji-option-${this.key}-${index}`;
+    this.props.ariaProps.ariaActiveDescendantID = this.props.ariaProps.ariaActiveDescendantID.set(this.key, descendant);
     this.setState({
       focusedOptionIndex: index,
     });
@@ -174,11 +151,45 @@ export default class EmojiSearch extends Component {
     return true;
   };
 
+  openDropdown = () => {
+    // This a really nasty way of attaching & releasing the key related functions.
+    // It assumes that the keyFunctions object will not loose its reference and
+    // by this we can replace inner parameters spread over different modules.
+    // This better be some registering & unregistering logic. PRs are welcome :)
+    this.props.callbacks.onDownArrow = this.props.callbacks.onDownArrow.set(this.key, this.onDownArrow);
+    this.props.callbacks.onUpArrow = this.props.callbacks.onUpArrow.set(this.key, this.onUpArrow);
+    this.props.callbacks.onEscape = this.props.callbacks.onEscape.set(this.key, this.onEscape);
+    this.props.callbacks.handleReturn = this.props.callbacks.handleReturn.set(this.key, this.commitSelection);
+    this.props.callbacks.onTab = this.props.callbacks.onTab.set(this.key, this.onTab);
+
+    const descendant = `emoji-option-${this.key}-${this.state.focusedOptionIndex}`;
+    this.props.ariaProps.ariaActiveDescendantID = this.props.ariaProps.ariaActiveDescendantID.set(this.key, descendant);
+    const owneeId = `emojis-list-${this.key}`;
+    this.props.ariaProps.ariaOwneeID = this.props.ariaProps.ariaOwneeID.set(this.key, owneeId);
+    this.props.ariaProps.ariaHasPopup = this.props.ariaProps.ariaHasPopup.set(this.key, true);
+    this.props.ariaProps.ariaExpanded = this.props.ariaProps.ariaExpanded.set(this.key, true);
+    this.setState({
+      isOpen: true,
+    });
+  };
+
   updateAriaCloseDropdown = () => {
-    this.props.ariaProps.ariaHasPopup = 'false';
-    this.props.ariaProps.ariaExpanded = 'false';
-    this.props.ariaProps.ariaActiveDescendantID = undefined;
-    this.props.ariaProps.ariaOwneeID = undefined;
+    this.props.ariaProps.ariaHasPopup = this.props.ariaProps.ariaHasPopup.delete(this.key);
+    this.props.ariaProps.ariaExpanded = this.props.ariaProps.ariaExpanded.delete(this.key);
+    this.props.ariaProps.ariaActiveDescendantID = this.props.ariaProps.ariaActiveDescendantID.delete(this.key);
+    this.props.ariaProps.ariaOwneeID = this.props.ariaProps.ariaOwneeID.delete(this.key);
+  };
+
+  closeDropdown = () => {
+    // make sure none of these callbacks are triggered
+    this.props.callbacks.onDownArrow = this.props.callbacks.onDownArrow.delete(this.key);
+    this.props.callbacks.onUpArrow = this.props.callbacks.onUpArrow.delete(this.key);
+    this.props.callbacks.onEscape = this.props.callbacks.onEscape.delete(this.key);
+    this.props.callbacks.handleReturn = this.props.callbacks.handleReturn.delete(this.key);
+    this.updateAriaCloseDropdown();
+    this.setState({
+      isOpen: false,
+    });
   };
 
   render() {
