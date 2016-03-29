e07686f2c5413b9df85521b0a4cdcfa09aa82ed0
Benjamin Kniffler <bkniffler@me.com>
2016-03-29 04:22:54 +0200

    refactor draggable wrapper

--- draft-js-dnd-plugin/src/components/block-draggable-wrapper.js
+++ draft-js-dnd-plugin/src/components/block-draggable-wrapper.js
@@ -1,34 +1,40 @@
 import React, {Component, PropTypes} from "react";
 
-class Wrapper extends Component {
-    static defaultProps = {
-        draggable: true,
-        readOnly: false
-    }
-    // Handle start-drag and setData with blockKey
-    startDrag(e){
-        var allow = this.props.draggable && !this.props.readOnly;
-        if(!allow) return;
-
-        e.dataTransfer.dropEffect = 'move';
-        // Declare data and give info that its an existing key and a block needs to be moved
-        e.dataTransfer.setData("text", 'key:'+this.props.block.key);
-    }
-    render(){
-        const {draggable, WrappedComponent} = this.props;
-        return (
-            <div onDragStart={::this.startDrag} draggable={draggable}>
-                <WrappedComponent {...this.props}/>
-            </div>
-        );
-    }
-};
-
 // Export
 export default function WrapComponent(component, options){
+    const {useDiv} = options||{};
+
+    class Wrapper extends Component {
+        static defaultProps = {
+            draggable: true,
+            readOnly: false
+        }
+        // Handle start-drag and setData with blockKey
+        startDrag(e){
+            var allow = this.props.draggable && !this.props.readOnly;
+            if(!allow) return;
+
+            e.dataTransfer.dropEffect = 'move';
+            // Declare data and give info that its an existing key and a block needs to be moved
+            e.dataTransfer.setData("text", 'key:'+this.props.block.key);
+        }
+        render(){
+            const {draggable, WrappedComponent} = this.props;
+            if(useDiv){
+                return (
+                    <div onDragStart={::this.startDrag} draggable={draggable}>
+                        <WrappedComponent {...this.props}/>
+                    </div>
+                );
+            }
+
+            return <WrappedComponent {...this.props} onDragStart={::this.startDrag} draggable={draggable}/>;
+        }
+    }
+
     return (props)=>{
         return (
-            <Wrapper {...props} {...options} WrappedComponent={component}></Wrapper>
+            <Wrapper {...props} WrappedComponent={component}></Wrapper>
         );
     }
 }
