f67c845765726795791224cea2592a5f40fd6bf0
Benjamin Kniffler <bkniffler@me.com>
2016-03-29 04:29:43 +0200

    Refactor onDropFile to add a preview block before upload

--- draft-js-dnd-plugin/src/modifiers/onDropFile.js
+++ draft-js-dnd-plugin/src/modifiers/onDropFile.js
@@ -1,41 +1,102 @@
 import AddBlock from './addBlock';
+import ModifyBlockData from './modifyBlockData';
+import {genKey, Entity} from "draft-js";
 
 export default function(config) {
     return function(e){
         const {props, selection, files, editorState, onChange} = e;
         // Get upload function from config or editor props
         const upload = config.upload || props.upload;
+        const progress = config.progress || props.progress || ((x)=>config.emitter.emit('progress', x));
         if (upload) {
-            //this.setState({fileDrag: false, uploading: true});
-            console.log('Starting upload');
-
             var formData = new FormData();
-            var data = {
-                files: []
-            };
+
+            // Set data {files: [Array of files], formData: FormData}
+            var data = {files: []};
             for (var key in files) {
                 formData.append('files', files[key]);
                 data.files.push(files[key]);
             }
             data.formData = data;
-            upload(data, (files, tag)=> {
-                console.log('Upload done');
-                // Success, tag can be function that returns editorState or a tag-type (default: image)
+
+            // Read files on client side
+            readFiles(data.files).then(files=>{
+                // Add blocks for each image before uploading
                 var newEditorState = editorState();
                 files.forEach(function (x) {
-                    newEditorState = typeof tag === 'function' ? tag(x) : AddBlock(newEditorState, selection, tag || 'image', x);
+                    newEditorState = AddBlock(newEditorState, selection, 'image', {...x, progress: 1});
                 });
                 onChange(newEditorState);
-            }, (err)=> {
-                // Failed
-                console.error(err)
-                //this.setState({uploading: false, uploadError: err});
-            }, (percent)=> {
-                // Progress
-                //this.setState({percent: percent !== 100 ? percent : null});
-                console.log(percent)
+
+                // Perform upload
+                upload(data, files => {
+                    // Success, remove 'progress' and 'src'
+                    var newEditorState = editorState();
+                    files.forEach(function (x) {
+                        var key = getBlockWhereEntityData(newEditorState, y=>y.src===x.src);
+                        if(key) {
+                            newEditorState = ModifyBlockData(newEditorState, key, {
+                                progress: undefined,
+                                src: undefined, ...x
+                            });
+                        }
+                    });
+                    // Propagate progress
+                    if(progress) progress(null);
+                    onChange(newEditorState);
+                }, (err)=> {
+                    console.error(err)
+                }, (percent)=> {
+                    // On progress, set entity data's progress field
+                    var newEditorState = editorState();
+                    files.forEach(function (x) {
+                        var key = getBlockWhereEntityData(newEditorState, y=>y.src===x.src);
+                        if(key){
+                            newEditorState = ModifyBlockData(newEditorState, key, {progress: percent});
+                        }
+                    });
+                    onChange(newEditorState);
+                    // Propagate progress
+                    if(progress) progress(percent);
+                });
             });
+
             return true;
         }
     }
+}
+
+function readFiles(files){
+    return Promise.all(files.map(readFile));
+}
+
+function readFile(file){
+    return new Promise((resolve, reject)=>{
+        var reader = new FileReader();
+        // This is called when finished reading
+        reader.onload = function (e) {
+            // Return an array with one image
+            resolve({
+                // These are attributes like size, name, type, ...
+                ...file,
+                // This is the files content as base64
+                src: e.target.result,
+                // No URL, since nothing on server
+                url: null
+            });
+        };
+        reader.readAsDataURL(file);
+    });
+}
+
+function getBlockWhereEntityData(state, query){
+    var map = state.getCurrentContent().get('blockMap').toArray();
+    for(var i=0; i<map.length; i++){
+        var block = map[i];
+        var entityData = block.getEntityAt(0) ? Entity.get(block.getEntityAt(0)).getData() : null;
+        if (block.get('type') === 'image' && entityData && query(entityData)) {
+            return block.get('key');
+        }
+    }
+    return null;
 }
