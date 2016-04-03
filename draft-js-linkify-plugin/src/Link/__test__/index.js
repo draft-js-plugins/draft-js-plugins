e3e10301fccbc9327e1c6c0268867de61f4129e2
Adrian Li <li.adrianmc@gmail.com>
2016-04-04 03:42:46 +0800

    added tests for the linkify plugin

--- draft-js-linkify-plugin/src/Link/__test__/index.js
+++ draft-js-linkify-plugin/src/Link/__test__/index.js
@@ -32,4 +32,34 @@ describe('Link', () => {
     const result = shallow(<Link decoratedText="https://www.draft-js-plugins.com/" />);
     expect(result).to.have.prop('href').to.contain('https://www.draft-js-plugins.com/');
   });
+
+  it('applies http prefix when none is supplied', () => {
+    const result = shallow(<Link decoratedText="draft-js-plugins.com/" />);
+    expect(result).to.have.prop('href').to.contain('http://draft-js-plugins.com/');
+  });
+
+  it('does not apply a prefix when one is already supplied', () => {
+    const result = shallow(<Link decoratedText="ftp://draft-js-plugins.com/" />);
+    expect(result).to.have.prop('href').to.contain('ftp://draft-js-plugins.com/');
+  });
+
+  it('generates correct href to localhost with port', () => {
+    const result = shallow(<Link decoratedText="http://localhost:8000" />);
+    expect(result).to.have.prop('href').to.contain('http://localhost:8000');
+  });
+
+  it('generates mailto href when supplied with email', () => {
+    const result = shallow(<Link decoratedText="name@example.com" />);
+    expect(result).to.have.prop('href').to.contain('mailto:name@example.com');
+  });
+
+  it('uses _self as the default target value', () => {
+    const result = shallow(<Link />);
+    expect(result).to.have.prop('target').to.contain('_self');
+  });
+
+  it('applies custom target value', () => {
+    const result = shallow(<Link target="_blank" />);
+    expect(result).to.have.prop('target').to.contain('_blank');
+  });
 });
