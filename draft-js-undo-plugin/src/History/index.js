ec33986e7509edae4c977def1f4c04ccd661d14d
Adrian Li <li.adrianmc@gmail.com>
2016-04-05 18:10:53 +0800

    refactored for better organization

--- draft-js-undo-plugin/src/History/index.js
+++ draft-js-undo-plugin/src/History/index.js
@@ -8,8 +8,9 @@ import HistoryEntry from './HistoryEntry';
 class History extends Component {
 
   generateHistoryEntries(stack) {
-    const { theme = Map(), className } = this.props; // eslint-disable-line no-use-before-define
+    const { theme = Map(), className } = this.props;
     const combinedClassName = unionClassNames(theme.get('historyItem'), className);
+
     return stack.map((entry) => (
       <HistoryEntry className={ combinedClassName }>
         { entry.getPlainText() }
@@ -18,11 +19,17 @@ class History extends Component {
   }
 
   render() {
-    const { theme = Map(), editorState = EditorState.createEmpty(), className } = this.props; // eslint-disable-line no-use-before-define
+    const {
+      theme = Map(),
+      editorState = EditorState.createEmpty(),
+      className,
+    } = this.props;
+
     const undoStack = editorState.getUndoStack();
     const redoStack = editorState.getRedoStack().reverse();
     const undoHistory = this.generateHistoryEntries(undoStack);
     const redoHistory = this.generateHistoryEntries(redoStack);
+
     const combinedClassName = unionClassNames(theme.get('historyItemActive'), className);
 
     return (
