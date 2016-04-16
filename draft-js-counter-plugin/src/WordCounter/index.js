2ce8a54899dd18ebc2a08233dedc754868838430
Adrian Li <li.adrianmc@gmail.com>
2016-04-16 19:20:35 +0800

    improved description and fixed missing whitespace bug

--- draft-js-counter-plugin/src/WordCounter/index.js
+++ draft-js-counter-plugin/src/WordCounter/index.js
@@ -12,8 +12,8 @@ class WordCounter extends Component {
   getWordCount(editorState) {
     const plainText = editorState.getCurrentContent().getPlainText('');
     const regex = /(?:\r\n|\r|\n)/g;  // new line, carriage return, line feed
-    const cleanString = plainText.replace(regex, '').trim();
-    const wordArray = cleanString.match(/\S+/g);
+    const cleanString = plainText.replace(regex, ' ').trim(); // replace above regex w/ space
+    const wordArray = cleanString.match(/\S+/g);  // matches words according to whitespace
     return wordArray ? wordArray.length : 0;
   }
 
