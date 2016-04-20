358867337083af3b3b38c53af810631340ac2775
Nik Graf <nik@nikgraf.com>
2016-04-20 01:26:47 -0700

    improve docs

--- docs/client/components/pages/Undo/index.js
+++ docs/client/components/pages/Undo/index.js
@@ -69,7 +69,7 @@ export default class App extends Component {
           <Heading level={ 2 }>Configuration Parameters</Heading>
           <div className={ styles.param }>
             <span className={ styles.paramName }>theme</span>
-            <span>Immutable.js Map of CSS classes with the following keys.</span>
+            <span>Object of CSS classes with the following keys.</span>
             <div className={ styles.subParams }>
               <div className={ styles.subParam }><span className={ styles.subParamName }>undo:</span> CSS class to be applied to undo button.</div>
               <div className={ styles.subParam }><span className={ styles.subParamName }>redo:</span> CSS class to be applied to redo button.</div>
