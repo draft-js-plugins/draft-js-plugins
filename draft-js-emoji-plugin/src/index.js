3258ec5b64bfd70e0f1534d7180d2b7ef2f0b65e
Nik Graf <nik@nikgraf.com>
2016-04-19 21:15:36 -0700

    avoid focus issues and double character changes

--- draft-js-emoji-plugin/src/index.js
+++ draft-js-emoji-plugin/src/index.js
@@ -94,11 +94,17 @@ const emojiPlugin = (config = {}) => {
     onChange: (editorState) => {
       let newEditorState = attachImmutableEntitiesToEmojis(editorState);
 
-      // Forcing the current selection ensures that it will be at it's right place.
-      // This solves the issue where inserting an Emoji on OSX with Apple's Emoji
-      // selector led to the right selection the data, but wrong position in
-      // the contenteditable.
-      newEditorState = EditorState.forceSelection(newEditorState, newEditorState.getSelection());
+      if (!newEditorState.getCurrentContent().equals(editorState.getCurrentContent())) {
+        // Forcing the current selection ensures that it will be at it's right place.
+        // This solves the issue where inserting an Emoji on OSX with Apple's Emoji
+        // selector led to the right selection the data, but wrong position in
+        // the contenteditable.
+        newEditorState = EditorState.forceSelection(
+          newEditorState,
+          newEditorState.getSelection(),
+        );
+      }
+
       if (callbacks.onChange.size !== 0) {
         callbacks.onChange.forEach((onChange) => {
           newEditorState = onChange(editorState);
