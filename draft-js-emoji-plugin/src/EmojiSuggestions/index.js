96ed2f5bd9fbee4fce77545830766e2e4b6c5c3e
Nik Graf <nik@nikgraf.com>
2016-04-29 16:26:46 +0200

    remove ontab listerner after closing the popup

--- draft-js-emoji-plugin/src/EmojiSuggestions/index.js
+++ draft-js-emoji-plugin/src/EmojiSuggestions/index.js
@@ -233,6 +233,7 @@ export default class EmojiSuggestions extends Component {
     // make sure none of these callbacks are triggered
     this.props.callbacks.onDownArrow = undefined;
     this.props.callbacks.onUpArrow = undefined;
+    this.props.callbacks.onTab = undefined;
     this.props.callbacks.onEscape = undefined;
     this.props.callbacks.handleReturn = undefined;
     this.props.ariaProps.ariaHasPopup = 'false';
