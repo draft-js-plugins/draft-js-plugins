0ce4a4ce023d876f2d0bc533fd35b26708504848
Benjamin Kniffler <bkniffler@me.com>
2016-04-29 15:17:03 +0200

    fix image plugin

--- draft-js-image-plugin/src/components/image.js
+++ draft-js-image-plugin/src/components/image.js
@@ -2,27 +2,25 @@ import React, { Component } from 'react';
 import imageStyles from '../style.css';
 
 class Image extends Component {
-  renderProgress = progress => {
-    if (progress >= 0) {
-      return (
-        <div className={ imageStyles.imageLoader } style={{ width: `${100 - progress}%` }} />
-      );
-    } return null;
+  static pluginOptions = {
+    customFocusedStyle: true,
   }
 
+  renderProgress = progress => progress >= 0
+    ? <div className={ imageStyles.imageLoader } style={{ width: `${100 - progress}%` }} />
+    : null;
+
   render() {
-    const { blockProps, alignment, onDragStart, draggable, actions } = this.props;
-    const { HoverToolbar } = blockProps;
+    const { blockProps, alignment, onDragStart, draggable } = this.props;
+    const { focused, progress, src, url } = blockProps;
     const className = `${imageStyles.imageWrapper} ${imageStyles[alignment || 'center']}`;
 
+    const style = focused ? { outline: '1px solid blue' } : null;
     return (
-        <figure className={className} contentEditable={false} onDragStart={onDragStart} draggable={draggable}
-          onClick={ blockProps.focus }
-        >
-          <img src={blockProps.src || blockProps.url} width="100%" height="auto" className={ imageStyles.image } />
-          {this.renderProgress(blockProps.progress)}
-          {HoverToolbar ? <HoverToolbar parent={ this } theme={blockProps.toolbarTheme} actions={ actions } /> : null}
-        </figure>
+      <figure className={className} contentEditable={false} onDragStart={onDragStart} draggable={draggable}>
+        <img src={src || url} width="100%" height="auto" className={imageStyles.image} style={style} />
+        {this.renderProgress(progress)}
+      </figure>
     );
   }
 }
