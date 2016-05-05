716683e700d832621ecd4e7b6e489e7907f397bd
Gal Schlezinger <galstar@spitfire.co.il>
2016-05-05 10:30:05 +0300

    Added configuration parameter, added docs

--- draft-js-emoji-plugin/src/Emoji/index.js
+++ draft-js-emoji-plugin/src/Emoji/index.js
@@ -3,12 +3,11 @@ import escapeMap from '../utils/escapeMap';
 import unionClassNames from 'union-class-names';
 import mappedUnicode from '../utils/mappedUnicode';
 
-const defaultImagePathSVG = '//cdn.jsdelivr.net/emojione/assets/svg/';
 const cacheBustParam = '?v=2.1.2';
 
 const Emoji = ({ theme = {}, imagePathSVG, className, decoratedText, ...props }) => {
   const unicode = escapeMap[decoratedText];
-  const imagePath = `url(${imagePathSVG || defaultImagePathSVG}${unicode}.svg${cacheBustParam})`;
+  const imagePath = `url(${imagePathSVG}${unicode}.svg${cacheBustParam})`;
   const combinedClassName = unionClassNames(theme.emoji, className);
   const characterClassName = unionClassNames(theme.emojiCharacter);
   return (
