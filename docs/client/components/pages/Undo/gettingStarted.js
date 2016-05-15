7e9345ff78ca8d07ab491a5651403d7f1f406e61
Nik Graf <nik@nikgraf.com>
2016-05-15 19:28:59 +0200

    fix docs for undo plugin

--- docs/client/components/pages/Undo/gettingStarted.js
+++ docs/client/components/pages/Undo/gettingStarted.js
@@ -1,20 +1,25 @@
 // It is important to import the Editor which accepts plugins.
 import Editor from 'draft-js-plugins-editor';
-import createHistoryPlugin from 'draft-js-history-plugin';
+import createUndoPlugin from 'draft-js-undo-plugin';
 import React from 'react';
 
 // Creates an Instance. At this step, a configuration object can be passed in
 // as an argument.
-const historyPlugin = createHistoryPlugin();
+const undoPlugin = createUndoPlugin();
+const { UndoButton, RedoButton } = undoPlugin;
 
-// The Editor accepts an array of plugins. In this case, only the historyPlugin
+// The Editor accepts an array of plugins. In this case, only the undoPlugin
 // is passed in, although it is possible to pass in multiple plugins.
 const MyEditor = ({ editorState, onChange }) => (
-  <Editor
-    editorState={ editorState }
-    onChange={ onChange }
-    plugins={ [historyPlugin] }
-  />
+  <div>
+    <Editor
+      editorState={ editorState }
+      onChange={ onChange }
+      plugins={ [undoPlugin] }
+    />
+    <UndoButton />
+    <RedoButton />
+  </div>
 );
 
 export default MyEditor;
