161182421697c7166aad129085ce5e29c236e601
Benjamin Kniffler <bkniffler@me.com>
2016-05-21 10:56:06 +0200

    cleanup-empty different method

--- draft-js-cleanup-empty-plugin/src/index.js
+++ draft-js-cleanup-empty-plugin/src/index.js
@@ -1,28 +1,29 @@
-import cleanupEmpty from './utils/cleanupEmpty';
+import onBackspace from './utils/onBackspace';
 
 // Block-Types to be handled will be stored here
 let types = [];
 const cleanupEmptyPlugin = options => {
   types = options.types || [];
   return {
-    onChange: editorState => {
-      let newEditorState = editorState;
+    handleKeyCommand(command, { getEditorState, setEditorState }) {
+      const editorState = getEditorState();
+      let newEditorState = null;
+      if (command.indexOf('backspace') === 0) {
+        newEditorState = onBackspace(editorState, types);
+      }
 
-      // Iterate over blockMap
-      editorState.getCurrentContent().get('blockMap').forEach(block => {
-        // If the block type is registered within the plugin, and no entity was
-        // found, perform cleanup of the block
-        if (types.indexOf(block.get('type')) !== -1 && block.getEntityAt(0) === null) {
-          newEditorState = cleanupEmpty(editorState, block.get('key'), block.get('type'));
-        }
-      });
-      return newEditorState;
+      if (newEditorState) {
+        setEditorState(newEditorState);
+        return true;
+      } return false;
     },
   };
 };
 
 export default cleanupEmptyPlugin;
+
 // Use this to add one type to the list
 export const cleanupType = item => types.push(item);
+
 // Use this to add multiple types to the list
 export const cleanupTypes = items => types.push(...items);
