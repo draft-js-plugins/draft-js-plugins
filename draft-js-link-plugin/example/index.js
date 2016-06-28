3860bd8826edc0c8655b1b7a30f64de22c93b6f7
Bryan Haakman <bryanhaakman@gmail.com>
2016-06-28 17:22:54 +0200

    add example for link plugin

---
+++ draft-js-link-plugin/example/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Editor from 'draft-js-plugins-editor';
+import createLinkPlugin from '../lib/index.js'
+import { EditorState } from 'draft-js';
+
+import './styles.css';
+
+const linkPlugin = createLinkPlugin()
+const { LinkButton } = linkPlugin
+
+class LinkEditor extends React.Component {
+    constructor(props) {
+        super(props)
+        this.onChange = this.onChange.bind(this)
+
+        this.state = {
+            editorState: EditorState.createEmpty()
+        }
+    }
+
+    onChange = (editorState) => {
+        this.setState({
+            editorState,
+        });
+    };
+
+    render() {
+        const {  } = this.props
+
+        return (
+            <div>
+                <LinkButton />
+                <Editor
+                    editorState={this.state.editorState}
+                    onChange={this.onChange}
+                    plugins={[linkPlugin]}
+                    ref="editor"
+                />
+            </div>
+            
+        )
+    }
+}
+
+ReactDOM.render((
+    <LinkEditor />
+), document.getElementById('target'));
---
+++ draft-js-link-plugin/example/webpack.config.js
@@ -0,0 +1,17 @@
+
+var webpack = require('webpack');
+var plugins = [];
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+    ]
+  },
+  plugins: plugins
+};
