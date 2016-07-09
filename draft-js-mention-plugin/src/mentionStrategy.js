71f08c60c409977acea1581fec340279ef16230b
Nik Graf <nik@nikgraf.com>
2016-07-09 08:48:13 +0200

    use "mention" type for @ trigger to avoid a breaking change

--- draft-js-mention-plugin/src/mentionStrategy.js
+++ draft-js-mention-plugin/src/mentionStrategy.js
@@ -1,8 +1,9 @@
 import { Entity } from 'draft-js';
+import getTypeByTrigger from './utils/getTypeByTrigger';
 
 const findMention = (trigger) => (character) => {
   const entityKey = character.getEntity();
-  return (entityKey !== null && Entity.get(entityKey).getType() === `mention-${trigger}`);
+  return (entityKey !== null && Entity.get(entityKey).getType() === getTypeByTrigger(trigger));
 };
 
 const findMentionEntities = (trigger) => (contentBlock, callback) => {
