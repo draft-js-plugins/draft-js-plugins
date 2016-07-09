d11616beb312a3177aea5dd0f59f743cba6e0850
Nik Graf <nik@nikgraf.com>
2016-07-09 09:02:27 +0200

    only import escapeRegExp instead of the whole lodash

--- draft-js-mention-plugin/src/mentionSuggestionsStrategy.js
+++ draft-js-mention-plugin/src/mentionSuggestionsStrategy.js
@@ -1,8 +1,8 @@
 /* @flow */
 
 import findWithRegex from 'find-with-regex';
-import _ from 'lodash';
+import escapeRegExp from 'lodash/escapeRegExp';
 
 export default (trigger: String) => (contentBlock: Object, callback: Function) => {
-  findWithRegex(new RegExp(`(\\s|^)${_.escapeRegExp(trigger)}[\\w]*`, 'g'), contentBlock, callback);
+  findWithRegex(new RegExp(`(\\s|^)${escapeRegExp(trigger)}[\\w]*`, 'g'), contentBlock, callback);
 };
