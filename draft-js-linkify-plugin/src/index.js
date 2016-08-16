8601aff81127af7a38751a911178dd814cbc3a9f
jonathan.liu <jonathan.liu@massdrop.com>
2016-08-16 11:14:24 -0700

    Configurable comoponent for linkify plugin

--- draft-js-linkify-plugin/src/index.js
+++ draft-js-linkify-plugin/src/index.js
@@ -1,26 +1,20 @@
 import Link from './Link';
 import linkStrategy from './linkStrategy';
-import styles from './styles.css';
 import decorateComponentWithProps from 'decorate-component-with-props';
 
-const defaultTheme = {
-  link: styles.link,
-};
-
 const linkPlugin = (config = {}) => {
-  // Styles are overwritten instead of merged as merging causes a lot of confusion.
-  //
-  // Why? Because when merging a developer needs to know all of the underlying
-  // styles which needs a deep dive into the code. Merging also makes it prone to
-  // errors when upgrading as basically every styling change would become a major
-  // breaking change. 1px of an increased padding can break a whole layout.
-  const theme = config.theme ? config.theme : defaultTheme;
+  const component = config.component;
   const target = config.target ? config.target : '_self';
+
+  if (!component) {
+    throw new Error('draft-js-linkify-plugin: Component not specified in initialization.');
+  }
+
   return {
     decorators: [
       {
         strategy: linkStrategy,
-        component: decorateComponentWithProps(Link, { theme, target }),
+        component: decorateComponentWithProps(Link, { component, target }),
       },
     ],
   };
