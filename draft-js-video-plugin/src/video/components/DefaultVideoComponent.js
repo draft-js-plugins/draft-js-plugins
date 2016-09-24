f3b8656b9d91043323cd3fed7176ec9dd29d797d
Ilkang Na <rivergod@users.noreply.github.com>
2016-09-24 09:03:47 +0900

    Update to support 16:9 ratio. (to do lint)
    Update to support 16:9 ratio. (to do lint)
    Update to support 16:9 ratio. (width: parrent width size)

--- draft-js-video-plugin/src/video/components/DefaultVideoComponent.js
+++ draft-js-video-plugin/src/video/components/DefaultVideoComponent.js
@@ -19,12 +19,12 @@ const DefaultVideoCompoent = (props) => {
 
   if (src) {
     return (
-      <div style={{ "width": "100%", "height": "0", "position": "relative", "padding-bottom": "56.25%" }}>
+      <div style={{ width: '100%', height: '0', position: 'relative', 'padding-bottom': '56.25%' }}>
         <iframe
-            style={{ "width": "100%", "height": "100%", "position": "absolute", "top": "0", "left": "0" }}
-            src={src}
-            frameBorder="0"
-            allowFullScreen
+          style={{ width: '100%', height: '100%', position: 'absolute', top: '0', left: '0' }}
+          src={src}
+          frameBorder="0"
+          allowFullScreen
         />
       </div>
     );
