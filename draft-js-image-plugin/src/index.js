1d8d82a3216505c6f4879b3cdc2534319ab7bf57
caipeichao <peichao.cai@fraudmetrix.cn>
2016-10-08 23:20:09 +0800

    support paste image

--- draft-js-image-plugin/src/index.js
+++ draft-js-image-plugin/src/index.js
@@ -1,10 +1,14 @@
 import Image from './components/image';
 import styles from './style.css';
+import {Entity, AtomicBlockUtils} from 'draft-js';
 
 const imagePlugin = (config = {}) => {
   const type = config.type || 'block-image';
   const theme = config.theme ? config.theme : styles;
-  const component = config.component || Image({ theme });
+  const component = config.component || Image({theme});
+  const upload = config.upload || function (x, callback) {
+      callback(x);
+    };
   return {
     // Handle 'block-image' block-type with Image component
     blockRendererFn: (contentBlock) => {
@@ -13,8 +17,49 @@ const imagePlugin = (config = {}) => {
         return {
           component
         };
-      } return undefined;
+      }
+      return undefined;
     },
+    handlePastedFiles(e, editor) {
+      // Get editor state
+      var editorState = editor.getEditorState();
+
+      // No image found
+      if (e.length == 0) {
+        console.error('no image found');
+        return;
+      }
+
+      // Only support one image
+      if (e.length != 1) {
+        console.error('only support one image');
+        return;
+      }
+
+      // get image
+      var image = e[0];
+
+      // get image blob data as data url
+      var reader = new FileReader();
+      reader.onload = function (event) {
+        // upload data url
+        var url = event.target.result;
+        upload(url, function (url2) {
+          // insert image
+          const entityKey = Entity.create('block-image', 'IMMUTABLE',
+            {
+              src: url2, progress: -1
+            }
+          );
+          editorState = AtomicBlockUtils.insertAtomicBlock(editorState, entityKey, ' ');
+
+          // Set editor state
+          editor.setEditorState(editorState);
+        });
+      };
+      reader.readAsDataURL(image);
+    }
+
   };
 };
 
