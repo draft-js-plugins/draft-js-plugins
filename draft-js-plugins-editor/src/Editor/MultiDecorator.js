8ef5888b27e4410af372bc2b51556b1ffcdf0b72
Max Winderbaum <maxwinderbaum@gmail.com>
2016-10-14 15:25:23 -0700

    Convert MultiDecorator to ES6 Class

--- draft-js-plugins-editor/src/Editor/MultiDecorator.js
+++ draft-js-plugins-editor/src/Editor/MultiDecorator.js
@@ -2,84 +2,84 @@ import Immutable from 'immutable';
 
 const KEY_SEPARATOR = '-';
 
-function MultiDecorator(decorators) {
-  this.decorators = Immutable.List(decorators);
-}
+class MultiDecorator {
+  constructor(decorators) {
+    this.decorators = Immutable.List(decorators);
+  }
 
-/**
- Return list of decoration IDs per character
+  /**
+   * Return list of decoration IDs per character
+   *
+   * @param {ContentBlock} block
+   * @return {List<String>}
+   */
+  getDecorations(block) {
+    const decorations = new Array(block.getText().length).fill(null);
 
- @param {ContentBlock} block
- @return {List<String>}
- */
-MultiDecorator.prototype.getDecorations = function getDecorations(block) {
-  const decorations = Array(block.getText().length).fill(null);
+    this.decorators.forEach((decorator, i) => {
+      const subDecorations = decorator.getDecorations(block);
 
-  this.decorators.forEach((decorator, i) => {
-    const subDecorations = decorator.getDecorations(block);
+      subDecorations.forEach((key, offset) => {
+        if (!key) {
+          return;
+        }
 
-    subDecorations.forEach((key, offset) => {
-      if (!key) {
-        return;
-      }
-
-      const decoration = i + KEY_SEPARATOR + key;
-
-      decorations[offset] = decoration;
+        decorations[offset] = i + KEY_SEPARATOR + key;
+      });
     });
-  });
 
-  return Immutable.List(decorations);
-};
+    return Immutable.List(decorations);
+  }
 
-/**
- Return component to render a decoration
+  /**
+   * Return component to render a decoration
+   *
+   * @param {String} key
+   * @return {Function}
+   */
+  getComponentForKey(key) {
+    const decorator = this.getDecoratorForKey(key);
+    return decorator.getComponentForKey(
+      MultiDecorator.getInnerKey(key)
+    );
+  }
 
- @param {String} key
- @return {Function}
- */
-MultiDecorator.prototype.getComponentForKey = function getComponentForKey(key) {
-  const decorator = this.getDecoratorForKey(key);
-  return decorator.getComponentForKey(
-    this.getInnerKey(key)
-  );
-};
+  /**
+   * Return props to render a decoration
+   *
+   * @param {String} key
+   * @return {Object}
+   */
+  getPropsForKey(key) {
+    const decorator = this.getDecoratorForKey(key);
+    return decorator.getPropsForKey(
+      MultiDecorator.getInnerKey(key)
+    );
+  }
 
-/**
- Return props to render a decoration
+  /**
+   * Return a decorator for a specific key
+   *
+   * @param {String} key
+   * @return {Decorator}
+   */
+  getDecoratorForKey(key) {
+    const parts = key.split(KEY_SEPARATOR);
+    const index = Number(parts[0]);
 
- @param {String} key
- @return {Object}
- */
-MultiDecorator.prototype.getPropsForKey = function getPropsForKey(key) {
-  const decorator = this.getDecoratorForKey(key);
-  return decorator.getPropsForKey(
-    this.getInnerKey(key)
-  );
-};
+    return this.decorators.get(index);
+  }
 
-/**
- Return a decorator for a specific key
-
- @param {String} key
- @return {Decorator}
- */
-MultiDecorator.prototype.getDecoratorForKey = function getDecoratorForKey(key) {
-  const parts = key.split(KEY_SEPARATOR);
-  const index = Number(parts[0]);
-
-  return this.decorators.get(index);
-};
-
-/**
- Return inner key for a decorator
-
- @param {String} key
- @return {String}
- */
-MultiDecorator.prototype.getInnerKey = function getInnerKey(key) {
-  const parts = key.split(KEY_SEPARATOR);
-  return parts.slice(1).join(KEY_SEPARATOR);
-};
+  /**
+   * Return inner key for a decorator
+   *
+   * @param {String} key
+   * @return {String}
+   */
+  static getInnerKey(key) {
+    const parts = key.split(KEY_SEPARATOR);
+    return parts.slice(1).join(KEY_SEPARATOR);
+  }
+}
 
 module.exports = MultiDecorator;
