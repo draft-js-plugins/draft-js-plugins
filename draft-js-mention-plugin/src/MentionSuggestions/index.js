317345c82eb44f4e0ec6b98ee343aea2a945fb0e
Yoonji Jang <erikafsatin@gmail.com>
2016-10-15 20:06:03 +0900

    remove unnecessary escape character

--- draft-js-mention-plugin/src/MentionSuggestions/index.js
+++ draft-js-mention-plugin/src/MentionSuggestions/index.js
@@ -19,7 +19,7 @@ export default class MentionSuggestions extends Component {
     suggestions: (props, propName, componentName) => {
       if (!List.isList(props[propName])) {
         return new Error(
-          `Invalid prop \`${propName}\' supplied to \`${componentName}\`. should be an instance of immutable list.`
+          `Invalid prop \`${propName}' supplied to \`${componentName}\`. should be an instance of immutable list.`
         );
       }
       return undefined;
