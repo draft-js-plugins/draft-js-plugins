fd72509547c8aae59adcf6b4cb023be84eed528d
Nik Graf <nik@nikgraf.com>
2016-10-19 23:40:49 +0200

    wip

--- docs/client/components/pages/Image/CustomImageEditor/index.js
+++ docs/client/components/pages/Image/CustomImageEditor/index.js
@@ -5,17 +5,18 @@ import Editor from 'draft-js-plugins-editor';
 // eslint-disable-next-line import/no-unresolved
 import createImagePlugin from 'draft-js-image-plugin';
 // eslint-disable-next-line import/no-unresolved
-// import createFocusPlugin from 'draft-js-focus-plugin';
+import createFocusPlugin from 'draft-js-focus-plugin';
 // eslint-disable-next-line import/no-unresolved
-import createResizeablePlugin from 'draft-js-resizeable-plugin';
+// import createResizeablePlugin from 'draft-js-resizeable-plugin';
 // eslint-disable-next-line import/no-unresolved
 import createEntityPropsPlugin from 'draft-js-entity-props-plugin';
 import editorStyles from './editorStyles.css';
 
-// const focusPlugin = createFocusPlugin();
-const resizeablePlugin = createResizeablePlugin();
+const focusPlugin = createFocusPlugin();
+// const resizeablePlugin = createResizeablePlugin();
 const entityPropsPlugin = createEntityPropsPlugin();
-const imagePlugin = createImagePlugin({ decorator: resizeablePlugin.Decorator });
+// const imagePlugin = createImagePlugin({ decorator: resizeablePlugin.Decorator });
+const imagePlugin = createImagePlugin({ decorator: focusPlugin.Decorator });
 const plugins = [entityPropsPlugin, imagePlugin];
 const { ImageAdd } = imagePlugin;
 
