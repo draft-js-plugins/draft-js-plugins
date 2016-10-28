10d2c2b9fb2d64461ff1b556d57cc47065b15fee
Anil Kulkarni <anil@textio.com>
2016-10-27 18:05:32 -0700

    Add editorRefChanged as a callback

--- draft-js-plugins-editor/src/Editor/index.js
+++ draft-js-plugins-editor/src/Editor/index.js
@@ -123,6 +123,15 @@ class PluginEditor extends Component {
     getEditorRef: this.getEditorRef,
   });
 
+  editorRefChanged = (editor) => {
+    this.editor = editor;
+    this.resolvePlugins().forEach((plugin) => {
+      if (plugin.onRefChanged) {
+        plugin.onRefChanged(editor);
+      }
+    });
+  };
+
   createEventHooks = (methodName, plugins) => (...args) => {
     const newArgs = [].slice.apply(args);
     newArgs.push(this.getPluginMethods());
@@ -332,7 +341,7 @@ class PluginEditor extends Component {
         blockRenderMap={blockRenderMap}
         onChange={this.onChange}
         editorState={this.props.editorState}
-        ref={(element) => { this.editor = element; }}
+        ref={this.editorRefChanged}
       />
     );
   }
