dcf618e3812109371793cea48df9bd484b761d4e
Nik Graf <nik@nikgraf.com>
2016-11-12 21:49:51 +0100

    wip alignment plugin

---
+++ draft-js-alignment-plugin/src/AlignmentTool/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => (
+  <div>
+    <div>Left</div>
+    <div>Center</div>
+    <div>Right</div>
+  </div>
+);
--- draft-js-alignment-plugin/src/index.js
+++ draft-js-alignment-plugin/src/index.js
@@ -1,5 +1,7 @@
 import { Entity, EditorState } from 'draft-js';
+import decorateComponentWithProps from 'decorate-component-with-props';
 import createDecorator from './createDecorator';
+import AlignmentTool from './AlignmentTool';
 
 const store = {
   getEditorRef: undefined,
@@ -17,22 +19,28 @@ const createSetAlignmentData = (contentBlock, { getEditorState, setEditorState }
   }
 };
 
-export default (config) => ({
-  initialize: ({ getEditorRef, getReadOnly, getEditorState, setEditorState }) => {
-    store.getReadOnly = getReadOnly;
-    store.getEditorRef = getEditorRef;
-    store.getEditorState = getEditorState;
-    store.setEditorState = setEditorState;
-  },
-  decorator: createDecorator({ config, store }),
-  blockRendererFn: (contentBlock, { getEditorState, setEditorState }) => {
-    const entityKey = contentBlock.getEntityAt(0);
-    const alignmentData = entityKey ? Entity.get(entityKey).data : {};
-    return {
-      props: {
-        alignmentData,
-        setAlignmentData: createSetAlignmentData(contentBlock, { getEditorState, setEditorState }),
-      },
-    };
-  }
-});
+export default (config) => {
+  const alignmentToolProps = {
+    store
+  };
+  return {
+    initialize: ({ getEditorRef, getReadOnly, getEditorState, setEditorState }) => {
+      store.getReadOnly = getReadOnly;
+      store.getEditorRef = getEditorRef;
+      store.getEditorState = getEditorState;
+      store.setEditorState = setEditorState;
+    },
+    decorator: createDecorator({ config, store }),
+    blockRendererFn: (contentBlock, { getEditorState, setEditorState }) => {
+      const entityKey = contentBlock.getEntityAt(0);
+      const alignmentData = entityKey ? Entity.get(entityKey).data : {};
+      return {
+        props: {
+          alignmentData,
+          setAlignmentData: createSetAlignmentData(contentBlock, { getEditorState, setEditorState }),
+        },
+      };
+    },
+    AlignmentTool: decorateComponentWithProps(AlignmentTool, alignmentToolProps),
+  };
+};
