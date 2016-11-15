0fcb3fdd556efb4e8e838601a1fe5d2237d539de
Nihau <nicolas.hauseux@gmail.com>
2016-11-15 15:39:59 +0100

    typo

--- draft-js-inline-toolbar-plugin/src/components/Toolbar/index.js
+++ draft-js-inline-toolbar-plugin/src/components/Toolbar/index.js
@@ -17,7 +17,7 @@ export default class Toolbar extends React.Component {
 
   onVisibilityChanged = (isVisible) => {
     // need to wait a tick for window.getSelection() to be accurate
-    // when focusing editor with allready present selection
+    // when focusing editor with already present selection
     setTimeout(() => {
       const selectionRect = isVisible ? getVisibleSelectionRect(window) : undefined;
       const position = selectionRect ? {
