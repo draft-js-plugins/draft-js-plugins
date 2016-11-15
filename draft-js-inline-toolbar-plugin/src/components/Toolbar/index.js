7dcf76ad887b58559e3ca2920bc9e84e25ef2b52
Nihau <nicolas.hauseux@gmail.com>
2016-11-15 15:05:51 +0100

    bugfix position toolbar on focus

--- draft-js-inline-toolbar-plugin/src/components/Toolbar/index.js
+++ draft-js-inline-toolbar-plugin/src/components/Toolbar/index.js
@@ -16,18 +16,22 @@ export default class Toolbar extends React.Component {
   }
 
   onVisibilityChanged = (isVisible) => {
-    const selectionRect = isVisible ? getVisibleSelectionRect(window) : undefined;
-    const position = selectionRect ? {
-      top: (selectionRect.top + window.scrollY) - toolbarHeight,
-      left: selectionRect.left + window.scrollX + (selectionRect.width / 2),
-      transform: 'translate(-50%) scale(1)',
-      transition: 'transform 0.15s cubic-bezier(.3,1.2,.2,1)',
-    } : {
-      transform: 'translate(-50%) scale(0)',
-    };
-    this.setState({
-      position,
-    });
+    // need to wait a tick for window.getSelection() to be accurate
+    // when focusing editor with allready present selection
+    setTimeout(() => {
+      const selectionRect = isVisible ? getVisibleSelectionRect(window) : undefined;
+      const position = selectionRect ? {
+        top: (selectionRect.top + window.scrollY) - toolbarHeight,
+        left: selectionRect.left + window.scrollX + (selectionRect.width / 2),
+        transform: 'translate(-50%) scale(1)',
+        transition: 'transform 0.15s cubic-bezier(.3,1.2,.2,1)',
+      } : {
+        transform: 'translate(-50%) scale(0)',
+      };
+      this.setState({
+        position,
+      });
+    }, 0);
   }
 
   render() {
