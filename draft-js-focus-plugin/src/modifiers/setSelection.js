5d2e7c73dcd2a4958cc4a1ba047485f8254ebfc1
Nik Graf <nik@nikgraf.com>
2016-12-08 18:18:22 +0100

    fix caret management for setSelection

--- draft-js-focus-plugin/src/modifiers/setSelection.js
+++ draft-js-focus-plugin/src/modifiers/setSelection.js
@@ -1,4 +1,5 @@
 import { SelectionState, EditorState } from 'draft-js';
+import DraftOffsetKey from 'draft-js/lib/DraftOffsetKey';
 
 const findParentNode = (node, filter) => {
   if (!node) return null;
@@ -19,15 +20,19 @@ export default (store, getEditorState, setEditorState, mode, event) => {
     return undefined;
   }
 
-  // const selection = window.getSelection();
-  // const range = document.createRange();
-  // range.setStart(startNode, 0);
-  // range.setEnd(endNode, 0);
-  // selection.removeAllRanges();
-  // selection.addRange(range);
-
-
   if (newActiveBlock) {
+    // TODO verify that always a key-0-0 exists
+    const offsetKey = DraftOffsetKey.encode(newActiveBlock.getKey(), 0, 0);
+    const node = document.querySelectorAll(`[data-offset-key="${offsetKey}"]`)[0];
+    // set the native selection to the node so the caret is not in the text and
+    // the selectionState matches the native selection
+    const selection = window.getSelection();
+    const range = document.createRange();
+    range.setStart(node, 0);
+    range.setEnd(node, 0);
+    selection.removeAllRanges();
+    selection.addRange(range);
+
     const offset = mode === 'up' ? newActiveBlock.getLength() : 0;
     event.preventDefault();
     setEditorState(EditorState.forceSelection(editorState, new SelectionState({
