007ec23211876105d42048fcc6873c9aae176714
mucciano <nicolas.eli.palmucci@gmail.com>
2017-01-30 16:18:39 -0500

    Improved documentation for SideToolbar added note about positioning.

--- docs/client/components/pages/SideToolbar/gettingStarted.js
+++ docs/client/components/pages/SideToolbar/gettingStarted.js
@@ -2,19 +2,23 @@
 import Editor from 'draft-js-plugins-editor'; // eslint-disable-line import/no-unresolved
 import createSideToolbarPlugin from 'draft-js-side-toolbar-plugin'; // eslint-disable-line import/no-unresolved
 import React from 'react';
-
+import editorStyles from './editorStyles.css';
 // Creates an Instance. At this step, a configuration object can be passed in
 // as an argument.
 const sideToolbarPlugin = createSideToolbarPlugin();
+const { SideToolbar } = sideToolbarPlugin;
 
 // The Editor accepts an array of plugins. In this case, only the sideToolbarPlugin
 // is passed in, although it is possible to pass in multiple plugins.
 const MyEditor = ({ editorState, onChange }) => (
-  <Editor
-    editorState={editorState}
-    onChange={onChange}
-    plugins={[sideToolbarPlugin]}
-  />
+  <div className={editorStyles.editor}>
+    <Editor
+      editorState={editorState}
+      onChange={onChange}
+      plugins={[sideToolbarPlugin]}
+    />
+    <SideToolbar />
+  </div>
 );
 
 export default MyEditor;
