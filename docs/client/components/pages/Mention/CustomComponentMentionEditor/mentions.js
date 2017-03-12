c9e0cc18f82f3fe1435d35fc7ec58539e916c293
Nik Graf <nik@nikgraf.com>
2017-03-12 15:43:13 +0100

    update images

--- docs/client/components/pages/Mention/CustomComponentMentionEditor/mentions.js
+++ docs/client/components/pages/Mention/CustomComponentMentionEditor/mentions.js
@@ -9,12 +9,12 @@ const mentions = fromJS([
   {
     name: 'julian',
     title: 'United Kingdom',
-    avatar: 'https://pbs.twimg.com/profile_images/477132877763579904/m5bFc8LF_400x400.png',
+    avatar: 'https://avatars2.githubusercontent.com/u/1188186?v=3&s=400',
   },
   {
     name: 'jyoti',
     title: 'New Delhi, India',
-    avatar: 'https://pbs.twimg.com/profile_images/705714058939359233/IaJoIa78_400x400.jpg',
+    avatar: 'https://avatars0.githubusercontent.com/u/2182307?v=3&s=400',
   },
   {
     name: 'max',
@@ -24,7 +24,7 @@ const mentions = fromJS([
   {
     name: 'nik',
     title: 'Passionate about Software Architecture, UX, Skiing & Triathlons',
-    avatar: 'https://pbs.twimg.com/profile_images/535634005769457664/Ppl32NaN_400x400.jpeg',
+    avatar: 'https://avatars0.githubusercontent.com/u/223045?v=3&s=400',
   },
   {
     name: 'pascal',
