82bc95a55202599bdd9c82cba02a2ce43e211a41
Jan Amann <jan@amann.me>
2017-05-09 18:30:40 +0200

    Add feature to toolbar that allows for children to override the content.

--- draft-js-inline-toolbar-plugin/src/components/Toolbar/index.js
+++ draft-js-inline-toolbar-plugin/src/components/Toolbar/index.js
@@ -22,15 +22,30 @@ export default class Toolbar extends React.Component {
 
   state = {
     isVisible: false,
+    position: undefined,
+
+    /**
+     * If this is set, the toolbar will render this instead of the regular
+     * structure and will also be shown when the editor loses focus. It's
+     * the responsibility of the callee to call this function again with
+     * `undefined` to show the regular structure again or hide the toolbar
+     * if the editor doesn't have focus anymore.
+     *
+     * @type {Component}
+     */
+    overrideContent: undefined
   }
 
   componentWillMount() {
-    this.props.store.subscribeToItem('isVisible', this.onVisibilityChanged);
+    this.props.store.subscribeToItem('selection', this.onSelectionChanged);
   }
 
   componentWillUnmount() {
-    this.props.store.unsubscribeFromItem('isVisible', this.onVisibilityChanged);
+    this.props.store.unsubscribeFromItem('selection', this.onSelectionChanged);
   }
+
+  onOverrideContent = (overrideContent) =>
+    this.setState({ overrideContent });
 
   onVisibilityChanged = (isVisible) => {
     // need to wait a tick for window.getSelection() to be accurate
@@ -52,24 +67,68 @@ export default class Toolbar extends React.Component {
       }
       this.setState({ position });
     }, 0);
+
+    // Revert back to the regular structure when the toolbar gets hidden
+    if (!isVisible && this.state.overrideContent) {
+      this.setState({ overrideContent: undefined });
+    }
   }
 
+  onSelectionChanged = (selection) => {
+    const relativeParent = getRelativeParent(this.toolbar.parentElement);
+    const relativeRect = (relativeParent || document.body).getBoundingClientRect();
+    const selectionRect = getVisibleSelectionRect(window);
+
+    if (!selectionRect) return;
+
+    const position = {
+      top: (selectionRect.top - relativeRect.top) - toolbarHeight,
+      left: (selectionRect.left - relativeRect.left) + (selectionRect.width / 2),
+    };
+    this.setState({ position });
+  };
+
+  getStyle() {
+    const { store } = this.props;
+    const { overrideContent, position } = this.state;
+    const selection = store.getItem('getEditorState')().getSelection();
+    const isVisible = !selection.isCollapsed() || overrideContent;
+    const style = { ...position };
+
+    if (isVisible) {
+      style.transform = 'translate(-50%) scale(1)';
+      style.transition = 'transform 0.15s cubic-bezier(.3,1.2,.2,1)';
+    } else {
+      style.transform = 'translate(-50%) scale(0)';
+    }
+
+    return style;
+  }
+
+  handleToolbarRef = (node) => {
+    this.toolbar = node;
+  };
+
   render() {
-    const { theme, store } = this.props;
+    const { theme, store, structure } = this.props;
+    const { overrideContent: OverrideContent } = this.state;
+    const childrenProps = {
+      theme: theme.buttonStyles,
+      getEditorState: store.getItem('getEditorState'),
+      setEditorState: store.getItem('setEditorState'),
+      onOverrideContent: this.onOverrideContent
+    };
+
     return (
       <div
         className={theme.toolbarStyles.toolbar}
-        style={this.state.position}
-        ref={(toolbar) => { this.toolbar = toolbar; }}
+        style={this.getStyle()}
+        ref={this.handleToolbarRef}
       >
-        {this.props.structure.map((Component, index) => (
-          <Component
-            key={index}
-            theme={theme.buttonStyles}
-            getEditorState={store.getItem('getEditorState')}
-            setEditorState={store.getItem('setEditorState')}
-          />
-        ))}
+        {OverrideContent
+          ? <OverrideContent {...childrenProps} />
+          : structure.map((Component, index) =>
+            <Component key={index} {...childrenProps} />)}
       </div>
     );
   }
