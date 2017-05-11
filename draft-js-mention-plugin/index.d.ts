faf03e1b1fb90e1bb0c2a1e8c5b58343eb16b832
echoulen <echoulen@gmail.com>
2017-05-11 15:11:14 +0800

    Add kaneoh-draft-js-mention-plugin typings

---
+++ draft-js-mention-plugin/index.d.ts
@@ -0,0 +1,4 @@
+declare module "kaneoh-draft-js-mention-plugin" {
+  export default function createUserMentionPlugin(arg : any, ...args);
+  export function defaultSuggestionsFilter(arg : any, ...args) : any;
+}
