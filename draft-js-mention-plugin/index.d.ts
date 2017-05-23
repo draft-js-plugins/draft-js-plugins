002b1cd9f46c6e77d5cb9b416b74dbb8b20eb4fc
echoulen <echoulen@gmail.com>
2017-05-23 10:28:54 +0800

    fix MentionProps type

--- draft-js-mention-plugin/index.d.ts
+++ draft-js-mention-plugin/index.d.ts
@@ -1,8 +1,9 @@
 export default function createUserMentionPlugin(arg : any, ...args);
 export function defaultSuggestionsFilter(arg : any, ...args) : any;
-export interface MentionProps {entityKey : any;
-    mention : Function;
+export interface MentionProps {
+    entityKey : any;
+    mention : any;
     theme : Object;
-    className : String;
+    className : string;
     decoratedText : string;
 }
