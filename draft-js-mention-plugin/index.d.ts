7565880104281d315cc43180ba93e23452068eb7
echoulen <echoulen@gmail.com>
2017-05-23 10:10:26 +0800

    kaneoh-draft-js-mention-plugin@0.0.9

--- draft-js-mention-plugin/index.d.ts
+++ draft-js-mention-plugin/index.d.ts
@@ -1,11 +1,8 @@
-declare module "kaneoh-draft-js-mention-plugin" {
-  export default function createUserMentionPlugin(arg : any, ...args);
-  export function defaultSuggestionsFilter(arg : any, ...args) : any;
-  export interface MentionProps {
-    entityKey : any;
+export default function createUserMentionPlugin(arg : any, ...args);
+export function defaultSuggestionsFilter(arg : any, ...args) : any;
+export interface MentionProps {entityKey : any;
     mention : Function;
     theme : Object;
     className : String;
     decoratedText : string;
-  }
 }
