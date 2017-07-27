9d723891a9d52c3dea415f129969e52fc125e654
Nik Graf <nik@nikgraf.com>
2017-07-27 11:23:21 +0200

    add publish script

---
+++ publish.sh
@@ -0,0 +1,60 @@
+cd draft-js-buttons
+npm publish --tag beta
+cd ..
+cd draft-js-alignment-plugin
+npm install
+npm publish --tag beta
+rm -rf node_modules
+cd ..
+cd draft-js-counter-plugin
+npm publish --tag beta
+cd ..
+cd draft-js-drag-n-drop-plugin
+npm publish --tag beta
+cd ..
+# cd draft-js-drag-n-drop-upload-plugin
+# npm publish --tag beta
+# cd ..
+cd draft-js-emoji-plugin
+npm publish --tag beta
+cd ..
+cd draft-js-focus-plugin
+npm publish --tag beta
+cd ..
+cd draft-js-hashtag-plugin
+npm publish --tag beta
+cd ..
+cd draft-js-image-plugin
+npm publish --tag beta
+cd ..
+cd draft-js-inline-toolbar-plugin
+npm install
+npm publish --tag beta
+rm -rf node_modules
+cd ..
+cd draft-js-linkify-plugin
+npm publish --tag beta
+cd ..
+cd draft-js-mention-plugin
+npm publish --tag beta
+cd ..
+cd draft-js-plugins-editor
+npm publish --tag beta
+cd ..
+cd draft-js-resizeable-plugin
+npm publish --tag beta
+cd ..
+cd draft-js-side-toolbar-plugin
+npm install
+npm publish --tag beta
+rm -rf node_modules
+cd ..
+cd draft-js-sticker-plugin
+npm publish --tag beta
+cd ..
+cd draft-js-undo-plugin
+npm publish --tag beta
+cd ..
+cd draft-js-video-plugin
+npm publish --tag beta
+cd ..
