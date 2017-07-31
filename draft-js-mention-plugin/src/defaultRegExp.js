98f67e0a11eae2485a9b8c36532d64f21c884e5f
Vadim Safonov <imamatory@users.noreply.github.com>
2017-07-31 15:09:38 +0300

    Add cyrillic symbols support in mention regexp

--- draft-js-mention-plugin/src/defaultRegExp.js
+++ draft-js-mention-plugin/src/defaultRegExp.js
@@ -4,4 +4,5 @@
 // For an advanced explaination about Hangul see https://github.com/draft-js-plugins/draft-js-plugins/pull/480#issuecomment-254055437
 // Hangul Syllables (korean): \uAC00-\uD7A3 - https://en.wikipedia.org/wiki/Korean_language_and_computers#Hangul_in_Unicode
 // Hangul Jamo (korean): \u3130-\u318F - https://en.wikipedia.org/wiki/Korean_language_and_computers#Hangul_in_Unicode
-export default '[\\w\u4e00-\u9eff\u3040-\u309F\u30A0-\u30FF\uAC00-\uD7A3\u3130-\u318F]*';
+// Cyrillic symbols: \u0410-\u044F - https://en.wikipedia.org/wiki/Cyrillic_script_in_Unicode
+export default '[\\w\u4e00-\u9eff\u3040-\u309F\u30A0-\u30FF\uAC00-\uD7A3\u3130-\u318F\u0410-\u044F]*';
