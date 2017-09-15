9eaa1f6281bef3ee43fec579a0895ae50aefb7bb
Eryk Napierala <eryk.napierala@allegrogroup.com>
2017-09-15 08:55:34 +0200

    remove dist files

--- draft-js-drag-n-drop-plugin/lib/constants/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var DRAFTJS_BLOCK_KEY = exports.DRAFTJS_BLOCK_KEY = 'DRAFTJS_BLOCK_KEY';
-var DRAFTJS_BLOCK_TYPE = exports.DRAFTJS_BLOCK_TYPE = 'DRAFTJS_BLOCK_TYPE';
