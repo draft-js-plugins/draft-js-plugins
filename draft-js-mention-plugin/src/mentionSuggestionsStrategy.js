ddbb566f44c8a70a385f4fb3864c3c9aa4c3c08e
Julian Krispel <julian@reactrocket.com>
2017-09-26 23:36:06 +0100

    fix mention suggestions strategy

--- draft-js-mention-plugin/src/mentionSuggestionsStrategy.js
+++ draft-js-mention-plugin/src/mentionSuggestionsStrategy.js
@@ -5,7 +5,7 @@ import escapeRegExp from 'lodash.escaperegexp';
 
 export default (trigger: string, regExp: string) => (contentBlock: Object, callback: Function) => {
   const reg = new RegExp(String.raw({
-    raw: `(\s|^)${escapeRegExp(trigger)}${regExp}` // eslint-disable-line no-useless-escape
+    raw: `(\\s|^)${escapeRegExp(trigger)}${regExp}` // eslint-disable-line no-useless-escape
   }), 'g');
   findWithRegex(reg, contentBlock, callback);
 };
