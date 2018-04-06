65e14222c88f585d99024476761bdb50cd96574c
Julian Krispel <julian@reactrocket.com>
2018-04-06 14:16:06 +0200

    fix webpack conf

--- draft-js-plugins-utils/webpack.config.js
+++ draft-js-plugins-utils/webpack.config.js
@@ -1,7 +1,11 @@
 /* eslint-disable no-var */
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
   output: {
     publicPath: '/',
     libraryTarget: 'commonjs2', // necessary for the babel plugin
   },
+  module: { loaders: [] },
 };
