19db7cb85521a9847a09e0b1043530f64a8a8cdd
Andrew Fan <andrew.fan@upsilonit.com>
2018-05-18 14:53:51 +0300

    check all blocks in selection

--- draft-js-inline-toolbar-plugin/src/utils/inCodeBlock.js
+++ draft-js-inline-toolbar-plugin/src/utils/inCodeBlock.js
@@ -1,11 +1,16 @@
 const inCodeBlock = (editorState) => {
   const startKey = editorState.getSelection().getStartKey();
-  if (startKey) {
-    const currentBlockType = editorState
-      .getCurrentContent()
-      .getBlockForKey(startKey)
-      .getType();
-    if (currentBlockType === 'code-block') return true;
+  const endKey = editorState.getSelection().getEndKey();
+  if (startKey && endKey) {
+    const contentState = editorState.getCurrentContent();
+    const blocks = contentState.getBlocksAsArray();
+
+    const selectedBlocks = blocks.slice(
+      Math.max(blocks.indexOf(contentState.getBlockForKey(startKey)) - 1, 0),
+      Math.min(blocks.indexOf(contentState.getBlockForKey(endKey)) + 1, blocks.length)
+    );
+
+    return selectedBlocks.find((block) => block.getType() === 'code-block');
   }
 
   return false;
