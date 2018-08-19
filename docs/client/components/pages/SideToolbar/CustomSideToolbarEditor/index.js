82a6fbe0ba841ce7f67eccf3ba115de92d6b3f23
freedomlang <blackywkl@gmail.com>
2018-08-20 00:06:10 +0800

    doc(side-toolbar): update doc for refactor via render prop

--- docs/client/components/pages/SideToolbar/CustomSideToolbarEditor/index.js
+++ docs/client/components/pages/SideToolbar/CustomSideToolbarEditor/index.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 
 import Editor, { createEditorStateWithText } from 'draft-js-plugins-editor';
+import {
+  HeadlineOneButton,
+  HeadlineTwoButton,
+  BlockquoteButton,
+  CodeBlockButton,
+  UnorderedListButton,
+  OrderedListButton,
+} from 'draft-js-buttons';
 
 import createSideToolbarPlugin from 'draft-js-side-toolbar-plugin';
 import editorStyles from './editorStyles.css';
@@ -40,7 +48,25 @@ export default class CustomSideToolbarEditor extends Component {
           plugins={plugins}
           ref={(element) => { this.editor = element; }}
         />
-        <SideToolbar />
+        <SideToolbar>
+          {(getEditorState, setEditorState, theme) => {
+            const buttonProps= {
+              getEditorState,
+              setEditorState,
+              theme
+            };
+
+            // may be use React.Fragment instead of div to improve perfomance after React 16
+            return (<div>
+              <HeadlineOneButton {...buttonProps}/>
+              <HeadlineTwoButton {...buttonProps}/>
+              <BlockquoteButton {...buttonProps}/>
+              <CodeBlockButton {...buttonProps}/>
+              <UnorderedListButton {...buttonProps}/>
+              <OrderedListButton {...buttonProps}/>
+            </div>)
+          }}
+        </SideToolbar>
       </div>
     );
   }
