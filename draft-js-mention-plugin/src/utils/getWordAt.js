b92e83795532bb9b55ea3bc27f9a0c6647842ef2
Danyu You <dyou@proofpoint.com>
2018-08-21 14:30:53 -0700

    Reorganized for readability.

--- draft-js-mention-plugin/src/utils/getWordAt.js
+++ draft-js-mention-plugin/src/utils/getWordAt.js
@@ -2,11 +2,16 @@ const getWordAt = (string, position) => {
   // Perform type conversions.
   const str = String(string);
   // eslint-disable-next-line no-bitwise
-  const pos = Number(position) >>> 0;
+  let pos = Number(position) >>> 0;
+
+  // Pos goes to end of word already when mentiontrigger is empty(''),
+  // so make sure adjusting pos ONLY when cursor pos is NOT at the end of word.
+  if (str.charAt(pos) !== ' ') {
+    pos += 1;
+  }
 
   // Search for the word's beginning and end.
-  // Do NOT adjust pos if it already at the end of word.(Happens when mentiontrigger is empty(''))
-  const left = str.slice(0, str.charAt(pos) === ' ' ? pos : pos + 1).search(/\S+$/);
+  const left = str.slice(0, pos).search(/\S+$/);
   const right = str.slice(pos).search(/\s/);
 
   // The last word in the string is a special case.
