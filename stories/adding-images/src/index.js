2272e0c0d529729228c58d68f798d6faded5fa0b
Julian Krispel <julian@reactrocket.com>
2018-08-27 15:41:48 +0100

    add react-scripts@next to add-images story

--- stories/adding-images/src/index.js
+++ stories/adding-images/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'draft-js/dist/Draft.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
