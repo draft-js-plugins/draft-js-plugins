f41f6a330cf13e39d85bc2572d8787ef26f809f9
Julian Krispel <julian@reactrocket.com>
2018-08-27 15:27:04 +0100

    transformed all stories into cra examples

--- stories/index.js
+++ stories/index.js
@@ -2,158 +2,79 @@ import React from 'react';
 import 'style-loader!css-loader!draft-js/dist/Draft.css'; // eslint-disable-line import/no-unresolved
 
 import { storiesOf } from '@storybook/react';
-// import { action } from '@storybook/addon-actions';
-// import { linkTo } from '@storybook/addon-links';
-
-// Alignment
-import SimpleAlignmentEditor from './simple-alignment-editor/src/App';
-
-// Anchor
-import AnchorSimpleLinkPluginEditor from './anchor-simple-inline-toolbar-editor/src/App';
-
-// Counter
-import SimpleCounterEditor from './counter-plugin-simple-editor/src/App';
-
-// DragNDrop
-import DragNDropImageEditor from './drag-n-drop-plugin-simple-editor/src/App';
-
-// Emoji
-import SimpleEmojiEditor from './emoji-plugin-simple-editor/src/App';
-import CustomEmojiEditor from './emoji-plugin-themed-editor/src/App';
-
-// Focus
-import SimpleFocusEditor from './focus-plugin-simple-editor/src/App';
-
-// Kitchen Sink
-import KitchenSink from './kitchensink-editor/src/App';
-
-// HashTag
-import SimpleHashtagEditor from './hashtag-plugin-simple-editor/src/App';
-import CustomHashtagEditor from './hashtag-plugin-themed-editor/src/App';
-
-// Image
+import SimpleAlignmentEditor from './alignment/src/App';
+import ResizableEditor from './resizable/src/App';
+import AnchorSimpleLinkPluginEditor from './inline-toolbar-with-anchor-plugin/src/App';
+import SimpleCounterEditor from './counter-plugin-simple/src/App';
+import DragNDropImageEditor from './drag-n-drop/src/App';
+import SimpleEmojiEditor from './emoji/src/App';
+import CustomEmojiEditor from './emoji-styled/src/App';
+import SimpleFocusEditor from './focus/src/App';
+import KitchenSink from './kitchensink/src/App';
+import SimpleHashtagEditor from './hashtag/src/App';
+import CustomHashtagEditor from './hashtag-styled/src/App';
 import SimpleImageEditor from './displaying-images/src/App';
 import CustomImageEditor from './align-drag-focus-and-resize-images/src/App';
 import AddImageEditor from './adding-images/src/App';
-
-// InlineToolbar
 import SimpleInlineToolbarEditor from './inline-toolbar/src/App';
-import CustomInlineToolbarEditor from './InlineToolbar/CustomInlineToolbarEditor';
-import ThemedInlineToolbarEditor from './InlineToolbar/ThemedInlineToolbarEditor';
-import RelativeParentInlineToolbarEditor from './InlineToolbar/RelativeParentInlineToolbarEditor';
+import CustomInlineToolbarEditor from './inline-toolbar-custom-buttons/src/App';
+import ThemedInlineToolbarEditor from './inline-toolbar-custom-styled/src/App';
+import RelativeParentInlineToolbarEditor from './inline-toolbar-relative-parent/src/App';
+import SimpleLinkifyEditor from './linkify/src/App';
+import CustomLinkifyEditor from './linkify-configured/src/App';
+import SimpleMentionEditor from './mentions/src/App';
+import RemoteMentionEditor from './mentions-remote/src/App';
+import CustomMentionEditor from './mentions-custom-styled/src/App';
+import CustomComponentMentionEditor from './mentions-custom-component/src/App';
+import MentionEditorWithCustomTrigger from './mentions-custom-trigger/src/App';
+import MentionEditorWithWhitespaceSupport from './mentions-with-whitespace/src/App';
+import SimpleSideToolbarEditor from './side-toolbar/src/App';
+import CustomSideToolbarEditor from './side-toolbar-custom/src/App';
+import RelativeParentSideToolbarEditor from './side-toolbar-relative-parent/src/App';
+import SimpleStickerEditor from './sticker/src/App';
+import SimpleUndoEditor from './undo/src/App';
+import SimpleVideoEditor from './video/src/App';
+import CustomAddVideoEditor from './adding-video/src/App';
+import CustomToolbarEditor from './static-toolbar-custom-buttons/src/App';
+import ThemedToolbarEditor from './static-toolbar-styled/src/App';
+import SimpleToolbarEditor from './static-toolbar/src/App';
+import DividerWithSideToolbarEditor from './divider-with-side-toolbar/src/App';
 
-// Linkify
-import SimpleLinkifyEditor from './Linkify/SimpleLinkifyEditor';
-import CustomLinkifyEditor from './Linkify/CustomLinkifyEditor';
-import CustomComponentLinkifyEditor from './Linkify/CustomComponentLinkifyEditor';
-
-// Mention
-import SimpleMentionEditor from './Mention/SimpleMentionEditor';
-import RemoteMentionEditor from './Mention/RemoteMentionEditor';
-import CustomMentionEditor from './Mention/CustomMentionEditor';
-import CustomComponentMentionEditor from './Mention/CustomComponentMentionEditor';
-import MentionEditorWithCustomTrigger from './Mention/MentionEditorWithCustomTrigger';
-import MentionEditorWithWhitespaceSupport from './Mention/MentionEditorWithWhitespaceSupport';
-
-// SideToolbar
-import SimpleSideToolbarEditor from './SideToolbar/SimpleSideToolbarEditor';
-import CustomSideToolbarEditor from './SideToolbar/CustomSideToolbarEditor';
-import RelativeParentSideToolbarEditor from './SideToolbar/RelativeParentSideToolbarEditor';
-
-// Sticker
-import SimpleStickerEditor from './Sticker/SimpleStickerEditor';
-import CustomStickerEditor from './Sticker/CustomStickerEditor';
-
-// Undo
-import SimpleUndoEditor from './Undo/SimpleUndoEditor';
-import CustomUndoEditor from './Undo/CustomUndoEditor';
-
-// Video
-import SimpleVideoEditor from './Video/SimpleVideoEditor';
-import CustomVideoEditor from './Video/CustomVideoEditor';
-import CustomAddVideoEditor from './Video/CustomAddVideoEditor';
-
-// Static toolbar
-import CustomToolbarEditor from './StaticToolbar/CustomToolbarEditor';
-import ThemedToolbarEditor from './StaticToolbar/ThemedToolbarEditor';
-import SimpleToolbarEditor from './StaticToolbar/SimpleToolbarEditor';
-
-// Divider
-import DividerWithSideToolbarEditor from './divider-with-side-toolbar-editor/src/App';
-
-storiesOf('Alignment Plugin', module)
-  .add('Editor with Alignment Plugin', () => <SimpleAlignmentEditor />);
-
-storiesOf('Anchor Plugin', module)
-  .add('Editor with Anchor Plugin', () => <AnchorSimpleLinkPluginEditor />);
-
-storiesOf('Counter Plugin', module)
-  .add('Editor with Counter Plugin', () => <SimpleCounterEditor />);
-
-storiesOf('DragNDrop Plugin', module)
-  .add('Editor with DragNDrop plugin and image plugin', () => <DragNDropImageEditor />);
-
-storiesOf('Emoji Plugin')
+storiesOf('Draft.js Plugins', module)
+  .add('Editor with Alignment Plugin', () => <SimpleAlignmentEditor />)
+  .add('Editor with Anchor Plugin', () => <AnchorSimpleLinkPluginEditor />)
+  .add('Editor with Counter Plugin', () => <SimpleCounterEditor />)
+  .add('Editor with DragNDrop plugin and image plugin', () => <DragNDropImageEditor />)
   .add('Editor with Emoji Plugin using emoji-one plugins', () => <SimpleEmojiEditor />)
-  .add('Editor with Emoji Plugin using native emojis', () => <CustomEmojiEditor />);
-
-storiesOf('Focus Plugin')
-  .add('Editor with Focus Plugin', () => <SimpleFocusEditor />);
-
-storiesOf('Hashtag Plugin')
+  .add('Editor with Emoji Plugin using native emojis', () => <CustomEmojiEditor />)
+  .add('Editor with Focus Plugin', () => <SimpleFocusEditor />)
   .add('Editor with Hashtag Plugin', () => <SimpleHashtagEditor />)
-  .add('Editor with custom themed Hashtag Plugin', () => <CustomHashtagEditor />);
-
-storiesOf('Image Plugin')
+  .add('Editor with custom themed Hashtag Plugin', () => <CustomHashtagEditor />)
   .add('Editor with Image Plugin', () => <SimpleImageEditor />)
   .add('Editor with Image Plugin and a few others: drag and drop, alignment, resizable, focus', () => <CustomImageEditor />)
-  .add('Editor with Image Plugin and Add mechanism', () => <AddImageEditor />);
-
-storiesOf('Inline Toolbar Plugin')
+  .add('Editor with Image Plugin and Add mechanism', () => <AddImageEditor />)
   .add('Editor with default inline toolbar plugin', () => <SimpleInlineToolbarEditor />)
   .add('Editor with inline toolbar plugin containing all buttons', () => <CustomInlineToolbarEditor />)
   .add('Editor with custom themed toolbar plugin', () => <ThemedInlineToolbarEditor />)
-  .add('Relatively positioned parent', () => <RelativeParentInlineToolbarEditor />);
-
-storiesOf('Linkify Plugin')
+  .add('Inline Toolbar with relatively positioned parent', () => <RelativeParentInlineToolbarEditor />)
   .add('Editor with Linkify Plugin', () => <SimpleLinkifyEditor />)
   .add('Editor with Linkify Plugin and configured to render links with - target: _blank', () => <CustomLinkifyEditor />)
-  .add('Editor with Linkify Plugin using a custom component to render the link', () => <CustomComponentLinkifyEditor />);
-
-storiesOf('Mention Plugin')
   .add('Editor with Mention Plugin', () => <SimpleMentionEditor />)
   .add('Editor with Mention Plugin and asynchronously loaded Suggestions', () => <RemoteMentionEditor />)
   .add('Editor with custom themed Mention Plugin', () => <CustomMentionEditor />)
-  .add('Editor with Mention Plugin and custom themed Suggestions', () => <CustomComponentMentionEditor />)
-  .add('Editor with mention trigger which needs to be escaped', () => <MentionEditorWithCustomTrigger />)
-  .add('Editor with support for matching suggestions with spaces', () => <MentionEditorWithWhitespaceSupport />);
-
-storiesOf('Side Toolbar Plugin')
-  .add('Editor with SideToolbar Plugin', () => <SimpleSideToolbarEditor />)
-  .add('Editor with custom themed SideToolbar Plugin', () => <CustomSideToolbarEditor />)
-  .add('Relatively positioned parent', () => <RelativeParentSideToolbarEditor />);
-
-storiesOf('Sticker Plugin')
+  .add('Mentions with styled suggestions', () => <CustomComponentMentionEditor />)
+  .add('Mentions with a custom trigger', () => <MentionEditorWithCustomTrigger />)
+  .add('Mentions with whitespace', () => <MentionEditorWithWhitespaceSupport />)
+  .add('Sidebar', () => <SimpleSideToolbarEditor />)
+  .add('Styled Sidebar', () => <CustomSideToolbarEditor />)
+  .add('Sidebar with relatively positioned parent', () => <RelativeParentSideToolbarEditor />)
   .add('Editor with Sticker Plugin', () => <SimpleStickerEditor />)
-  .add('Editor with custom themed Sticker Plugin', () => <CustomStickerEditor />);
-
-storiesOf('Undo Plugin')
   .add('Editor with Undo Plugin', () => <SimpleUndoEditor />)
-  .add('Editor with custom themed Undo Plugin', () => <CustomUndoEditor />);
-
-storiesOf('Video Plugin')
   .add('Editor with Video Plugin', () => <SimpleVideoEditor />)
-  .add('Editor with custom themed Video Plugin', () => <CustomVideoEditor />)
-  .add('Editor with Video Plugin and Add Video Button', () => <CustomAddVideoEditor />);
-
-
-storiesOf('Kitchen Sink')
-  .add('Kitchen Sink', () => <KitchenSink />);
-
-storiesOf('StaticToolbar Plugin')
+  .add('Editor with Video Plugin and Add Video Button', () => <CustomAddVideoEditor />)
+  .add('Kitchen Sink', () => <KitchenSink />)
+  .add('Resizable Editor', () => <ResizableEditor />)
   .add('CustomToolbarEditor', () => <CustomToolbarEditor />)
   .add('Simple toolbar editor', () => <SimpleToolbarEditor />)
-  .add('ThemedToolbarEditor', () => <ThemedToolbarEditor />);
-
-storiesOf('Divider Plugin')
+  .add('ThemedToolbarEditor', () => <ThemedToolbarEditor />)
   .add('Divider with SideToolbar', () => <DividerWithSideToolbarEditor />);
