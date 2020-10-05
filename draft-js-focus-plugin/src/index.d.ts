374adac78512baba3f89c5587a79aeda07133edf
M. Fadly Ramadhan <52827484+mfadlyramadhan@users.noreply.github.com>
2020-10-05 18:54:25 +0700

    Fix create focus plugin typings (#1506)

--- draft-js-focus-plugin/src/index.d.ts
+++ draft-js-focus-plugin/src/index.d.ts
@@ -3,6 +3,13 @@ import { EditorPlugin } from "draft-js-plugins-editor";
 
 type FocusEditorPlugin = EditorPlugin & { decorator: DraftDecorator };
 
-declare const createFocusPlugin: () => FocusEditorPlugin;
+export interface FocusEditorPluginConfig {
+  theme?: {
+    focused?: string;
+    unfocused?: string;
+  }
+}
+
+declare const createFocusPlugin: (config?: FocusEditorPluginConfig) => FocusEditorPlugin;
 
 export default createFocusPlugin;
