785e77588c6be63825deb108288a7a6ba7df0472
fxOne <fxOne@users.noreply.github.com>
2020-10-30 12:25:07 +0100

    migrate plugin

--- draft-js-counter-plugin/src/CharCounter/index.tsx
+++ draft-js-counter-plugin/src/CharCounter/index.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import punycode from 'punycode';
+import { EditorState } from 'draft-js';
+import { CounterPluginTheme } from '../theme';
+import { CounterPluginStore } from '..';
 
-const CharCounter = ({ theme = {}, className, store, limit }) => {
-  const getCharCount = editorState => {
-    const decodeUnicode = str => punycode.ucs2.decode(str); // func to handle unicode characters
+export interface CharCounterPubProps {
+  className?: string;
+  limit?: number;
+}
+
+interface CharCounterProps extends CharCounterPubProps {
+  theme?: CounterPluginTheme;
+  store: CounterPluginStore;
+}
+
+const CharCounter = ({
+  theme = {},
+  className,
+  store,
+  limit,
+}: CharCounterProps): ReactElement => {
+  const getCharCount = (editorState: EditorState): number => {
+    const decodeUnicode = (str: string): number[] => punycode.ucs2.decode(str); // func to handle unicode characters
     const plainText = editorState.getCurrentContent().getPlainText('');
     const regex = /(?:\r\n|\r|\n)/g; // new line, carriage return, line feed
     const cleanString = plainText.replace(regex, '').trim(); // replace above characters w/ nothing
     return decodeUnicode(cleanString).length;
   };
 
-  const getClassNames = count => {
+  const getClassNames = (count: number): string => {
     const defaultStyle = clsx(theme.counter, className);
     const overLimitStyle = clsx(theme.counterOverLimit, className);
     return count > limit ? overLimitStyle : defaultStyle;
   };
 
-  const count = getCharCount(store.getEditorState());
+  const count = getCharCount(store.getEditorState!());
   const classNames = getClassNames(count);
 
   return <span className={classNames}>{count}</span>;
