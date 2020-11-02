455bc190b2a8d6d13554a269503a403971444fda
fxOne <fxOne@users.noreply.github.com>
2020-11-02 13:08:15 +0100

    convert to typescript

--- draft-js-image-plugin/src/modifiers/addImage.ts
+++ draft-js-image-plugin/src/modifiers/addImage.ts
@@ -1,6 +1,10 @@
 import { EditorState, AtomicBlockUtils } from 'draft-js';
 
-export default (editorState, url, extraData) => {
+export default (
+  editorState: EditorState,
+  url: string,
+  extraData: Record<string, unknown>
+): EditorState => {
   const urlType = 'IMAGE';
   const contentState = editorState.getCurrentContent();
   const contentStateWithEntity = contentState.createEntity(
