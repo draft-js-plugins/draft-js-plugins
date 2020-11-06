5715557323227ccd77656e4028b46764de5df118
fxOne <fxOne@users.noreply.github.com>
2020-11-06 15:36:09 +0100

    clear timeout via ref

--- draft-js-alignment-plugin/src/AlignmentTool.tsx
+++ draft-js-alignment-plugin/src/AlignmentTool.tsx
@@ -1,5 +1,11 @@
 /* eslint-disable react/no-array-index-key */
-import React, { useState, useEffect, useRef, ReactElement } from 'react';
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  ReactElement,
+  useCallback,
+} from 'react';
 import {
   AlignBlockDefaultButton,
   AlignBlockLeftButton,
@@ -34,54 +40,55 @@ export default function AlignmentTool(props: AlignmentToolProps): ReactElement {
   const [position, setPosition] = useState({});
   const [alignment, setAlignment] = useState<string | null>(null);
   const toolbar = useRef<HTMLDivElement>(null);
-  const [timeoutHandle, setClearTimeout] = useState<
-    ReturnType<typeof setTimeout>
-  >();
+  const ref = useRef<ReturnType<typeof setTimeout>>();
 
-  const onVisibilityChanged = (visibleBlock?: null | string): void => {
-    const clear = setTimeout(() => {
-      let newPosition;
-      const boundingRect = props.store.getItem('boundingRect');
-      if (visibleBlock && boundingRect) {
-        const relativeParent = getRelativeParent(
-          toolbar.current!.parentElement
-        );
-        const toolbarHeight = toolbar.current!.clientHeight;
-        const relativeRect = relativeParent
-          ? relativeParent.getBoundingClientRect()
-          : document.body.getBoundingClientRect();
-        newPosition = {
-          top: boundingRect.top - relativeRect.top - toolbarHeight,
-          left: boundingRect.left - relativeRect.left + boundingRect.width / 2,
-          transform: 'translate(-50%) scale(1)',
-          transition: 'transform 0.15s cubic-bezier(.3,1.2,.2,1)',
-        };
-      } else {
-        newPosition = { transform: 'translate(-50%) scale(0)' };
-      }
-      const newAlignment = props.store.getItem('alignment') || 'default';
+  const onVisibilityChanged = useCallback(
+    (visibleBlock?: null | string): void => {
+      const clear = setTimeout(() => {
+        let newPosition;
+        const boundingRect = props.store.getItem('boundingRect');
+        if (visibleBlock && boundingRect) {
+          const relativeParent = getRelativeParent(
+            toolbar.current!.parentElement
+          );
+          const toolbarHeight = toolbar.current!.clientHeight;
+          const relativeRect = relativeParent
+            ? relativeParent.getBoundingClientRect()
+            : document.body.getBoundingClientRect();
+          newPosition = {
+            top: boundingRect.top - relativeRect.top - toolbarHeight,
+            left:
+              boundingRect.left - relativeRect.left + boundingRect.width / 2,
+            transform: 'translate(-50%) scale(1)',
+            transition: 'transform 0.15s cubic-bezier(.3,1.2,.2,1)',
+          };
+        } else {
+          newPosition = { transform: 'translate(-50%) scale(0)' };
+        }
+        const newAlignment = props.store.getItem('alignment') || 'default';
+        setAlignment(newAlignment);
+        setPosition(newPosition);
+        ref.current = undefined;
+      }, 0);
+      ref.current = clear;
+    },
+    []
+  );
 
-      setAlignment(newAlignment);
-      setPosition(newPosition);
-      setClearTimeout(undefined);
-    }, 0);
-    setClearTimeout(clear);
-  };
-
-  const onAlignmentChange = (value: string | undefined): void => {
+  const onAlignmentChange = useCallback((value: string | undefined): void => {
     if (value) {
       setAlignment(value);
     }
-  };
+  }, []);
 
   useEffect(
     () => () => {
       //clear timeout on unmount
-      if (timeoutHandle) {
-        clearTimeout(timeoutHandle);
+      if (ref.current) {
+        clearTimeout(ref.current);
       }
     },
-    [timeoutHandle]
+    []
   );
 
   useEffect(() => {
@@ -92,7 +99,7 @@ export default function AlignmentTool(props: AlignmentToolProps): ReactElement {
       props.store.unsubscribeFromItem('visibleBlock', onVisibilityChanged);
       props.store.unsubscribeFromItem('alignment', onAlignmentChange);
     };
-  }, []);
+  }, [onVisibilityChanged, onAlignmentChange]);
 
   const defaultButtons: DraftJsBlockAlignmentButtonType[] = [
     AlignBlockDefaultButton,
