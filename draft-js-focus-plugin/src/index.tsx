f2e0dbe99d5979e89517c1b110c09b8f19b23189
hcomPullReq <73121913+hcomPullReq@users.noreply.github.com>
2020-11-06 11:30:44 +0100

    convert to typescript (#1552)
    Co-authored-by: fxOne <fxOne@users.noreply.github.com>

--- draft-js-focus-plugin/src/index.tsx
+++ draft-js-focus-plugin/src/index.tsx
@@ -155,14 +155,14 @@ export default (config: FocusEditorPluginConfig = {}): FocusEditorPlugin => {
         // arrow down
         if (evt.keyCode === 40) {
           setSelection(getEditorState, setEditorState, 'down', evt);
-          return undefined;
+          return null;
         }
       }
 
       // Don't manually overwrite in case the shift key is used to avoid breaking
       // native behaviour that works anyway.
       if (evt.shiftKey) {
-        return undefined;
+        return null;
       }
 
       // arrow left
@@ -229,7 +229,7 @@ export default (config: FocusEditorPluginConfig = {}): FocusEditorPlugin => {
           setSelection(getEditorState, setEditorState, 'down', evt);
         }
       }
-      return undefined;
+      return null;
     },
 
     // Wrap all block-types in block-focus decorator
