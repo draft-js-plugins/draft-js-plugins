2e63d9bc624675207c369315ba2682e2d3edff27
Dominic Buetow <fxOne@users.noreply.github.com>
2021-02-23 22:05:37 +0100

    fixing prettier config issue (#1831)

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
       extends: [
         'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
-        'prettier/@typescript-eslint',
+        'prettier',
       ],
       files: ['**/*.ts', '**/*.tsx'],
       parser: '@typescript-eslint/parser',
