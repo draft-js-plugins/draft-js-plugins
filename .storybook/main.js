3b608dfbc810e513b6edef5913e4677c044c5e8a
fxOne <fxOne@users.noreply.github.com>
2021-05-29 17:17:30 +0200

    fixing issue with incorrect sizes

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,6 @@
 module.exports = {
   stories: ['../stories/index.js', '../stories/**/*.stories.*'],
+  typescript: {
+    reactDocgen: 'none', //see https://github.com/styleguidist/react-docgen-typescript/issues/356
+  },
 };
