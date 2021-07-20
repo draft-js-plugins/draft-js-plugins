79091a01855ba81d054f9f32307130adde5601e0
fxOne <fxOne@users.noreply.github.com>
2021-07-20 21:25:49 +0200

    fixing raw-loader definition

--- packages/docs/next-env.d.ts
+++ packages/docs/next-env.d.ts
@@ -1,8 +1,3 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
-
-declare module '!!raw-loader!*' {
-  const contents: string;
-  export default contents;
-}
