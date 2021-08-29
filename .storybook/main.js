625e5ae93cdb57c31d677f69358720526e6b4e70
fxOne <fxOne@users.noreply.github.com>
2021-08-29 12:15:19 +0200

    downgrade prepend-http

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,3 @@
 module.exports = {
   stories: ['../stories/index.js', '../stories/**/*.stories.*'],
-  typescript: {
-    reactDocgen: 'none', //see https://github.com/styleguidist/react-docgen-typescript/issues/356
-  },
 };
