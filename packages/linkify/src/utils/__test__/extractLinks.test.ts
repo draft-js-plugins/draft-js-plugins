c134bc4d1a49ad07a9ff21eddef54f66522ac18b
Sohaib Alqasem <sohaib_alqasem@yahoo.com>
2021-10-07 20:45:44 +0300

    - delete unUsed tests from extractLinks test - add test for link

--- packages/linkify/src/utils/__test__/extractLinks.test.ts
+++ packages/linkify/src/utils/__test__/extractLinks.test.ts
@@ -1,5 +1,3 @@
-import LinkifyIt from 'linkify-it';
-import tlds from 'tlds';
 import { extractLinks } from '../extractLinks';
 
 test.each([
@@ -16,34 +14,6 @@ test.each([
   ],
 ])('%s', (_description, text, result) => {
   const match = extractLinks(text);
-  expect(
-    match
-      ? match.map(({ index, lastIndex, raw }) => ({ index, lastIndex, raw }))
-      : match
-  ).toEqual(result);
-});
-
-test.each([
-  [
-    'test empty string with custom linkify-It configurations [emails exlucded]',
-    '',
-    null,
-  ],
-  [
-    'text without link with custom linkify-It configurations [emails exlucded]',
-    'Lorem ipsum dolor sit amet, consectetur testme@test.com adipiscing elit. Vivamus tempor, elit eu auctor dapibus, nisi massa ultricies turpis, vitae egestas',
-    null,
-  ],
-  [
-    'text with link with custom linkify-It configurations [emails exlucded]',
-    'Lorem ipsum dolor sit amet, www.google.de consectetur testme@test.com adipiscing elit. Vivamus tempor, elit eu auctor dapibus, nisi massa ultricies turpis, vitae egestas',
-    [{ index: 28, lastIndex: 41, raw: 'www.google.de' }],
-  ],
-])('%s', (_description, text, result) => {
-  const match = extractLinks(
-    text,
-    LinkifyIt().tlds(tlds).set({ fuzzyEmail: false })
-  );
   expect(
     match
       ? match.map(({ index, lastIndex, raw }) => ({ index, lastIndex, raw }))
