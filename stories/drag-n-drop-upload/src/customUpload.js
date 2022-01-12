bab662490495b07abe51b883d637580d63a58375
fcaminiti <federicodamiancaminiti@gmail.com>
2022-01-12 00:01:21 -0300

    Document custom upload function.

--- stories/drag-n-drop-upload/src/customUpload.js
+++ stories/drag-n-drop-upload/src/customUpload.js
@@ -4,10 +4,10 @@ const FILE_SRC = '/images/canada-landscape-small.jpg';
 *
 * @name: handleUpload
 * @desc: Custom file upload function.
-* @param {param type} data - (){})
-* @param {param type} success - successful upload
-* @param {param type} failed - failed to upload files.
-* @param {param type} progress - handle progress.
+* @param {[File], formData} data: consists of an array of files that have been uploaded and a formData object composed of those same files.
+* @param {function([{name: string, src: string}])} success - function to mark a successfull file upload, it takes an array of successfully uploaded files.
+* @param {function({name: string, src?: string})} failed - function that is called to mark a failure to upload one or more files. Removes the upload placeholders.
+* @param {(function(percent:int, file: {name:string, src:string})} progress - function to mark the progress in percentage points. It updates the progress count on each placeholder.
 */
 export const customUpload = (data, success, failed, progress):void => {
 
