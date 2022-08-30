801d5d2ecb972d4add6ace98cf9be9fe2afd140a
Shaan Mistry <97423936+mistryshaan@users.noreply.github.com>
2022-08-31 01:25:56 +0530

    Autoclose emoji popover for multiple instance (#2884)
    * Autoclose emoji popover for multiple instance
    
    * fixing types
    
    * closes #2875
    
    Co-authored-by: fxOne <fxOne@users.noreply.github.com>

--- packages/emoji/src/components/EmojiSelect/index.tsx
+++ packages/emoji/src/components/EmojiSelect/index.tsx
@@ -1,20 +1,19 @@
+import PropTypes from 'prop-types';
 import React, {
   Component,
   ComponentType,
-  MouseEvent,
   ReactElement,
   ReactNode,
 } from 'react';
-import PropTypes from 'prop-types';
-import createEmojisFromStrategy from '../../utils/createEmojisFromStrategy';
 import defaultEmojiGroups from '../../constants/defaultEmojiGroups';
-import Popover from './Popover';
 import {
   EmojiImageProps,
   EmojiPluginStore,
   EmojiPluginTheme,
   EmojiSelectGroup,
 } from '../../index';
+import createEmojisFromStrategy from '../../utils/createEmojisFromStrategy';
+import Popover from './Popover';
 
 const emojis = createEmojisFromStrategy();
 
@@ -66,20 +65,18 @@ export default class EmojiSelect extends Component<EmojiSelectParams> {
     isOpen: false,
   };
 
+  // Emoji select ref
+  emojiSelectRef = React.createRef<HTMLDivElement>();
+
   // When the selector is open and users click anywhere on the page,
   // the selector should close
   componentDidMount(): void {
-    document.addEventListener('click', this.closePopover);
+    document.addEventListener('click', this.closeIfClickedOutside);
   }
 
   componentWillUnmount(): void {
-    document.removeEventListener('click', this.closePopover);
+    document.removeEventListener('click', this.closeIfClickedOutside);
   }
-
-  onClick = (event: MouseEvent): void => {
-    event.stopPropagation();
-    event.nativeEvent.stopImmediatePropagation();
-  };
 
   onButtonMouseUp = (): void =>
     this.state.isOpen ? this.closePopover() : this.openPopover();
@@ -108,6 +105,17 @@ export default class EmojiSelect extends Component<EmojiSelectParams> {
     }
   };
 
+  // To check if clicked outside of emoji popover
+  closeIfClickedOutside = (e: MouseEvent): void => {
+    if (
+      this.emojiSelectRef.current &&
+      e.target instanceof Element &&
+      !this.emojiSelectRef.current.contains(e.target)
+    ) {
+      this.closePopover();
+    }
+  };
+
   render(): ReactElement {
     const {
       theme = {},
@@ -123,7 +131,7 @@ export default class EmojiSelect extends Component<EmojiSelectParams> {
       ? theme.emojiSelectButtonPressed
       : theme.emojiSelectButton;
     return (
-      <div className={theme.emojiSelect} onClick={this.onClick}>
+      <div className={theme.emojiSelect} ref={this.emojiSelectRef}>
         <button
           className={buttonClassName}
           onMouseUp={this.onButtonMouseUp}
