8c20eb5d5548a2cc9b20ca89947933958a67a903
Marco Burrometo <marco.burrometo@gmail.com>
2023-01-08 11:15:30 +0100

    Use for loop

--- packages/emoji/src/utils/shortnameToUnicode.ts
+++ packages/emoji/src/utils/shortnameToUnicode.ts
@@ -2,20 +2,22 @@ import { toShort } from 'emoji-toolkit';
 import data from 'emojibase-data/en/compact.json';
 
 const mapShortnameToUnicode: { key: string; value: string }[] = [];
-data.forEach((item) => {
+
+for (const item of data) {
   mapShortnameToUnicode.push({
     key: toShort(item.unicode),
     value: item.unicode,
   });
   if (item.skins) {
-    item.skins.forEach((skin) => {
+    for (const skin of item.skins) {
       mapShortnameToUnicode.push({
         key: toShort(skin.unicode),
         value: skin.unicode,
       });
-    });
+    }
   }
-});
+}
+
 export default function shortnameToUnicode(str: string): string {
   return str.length
     ? // Somehow encoding is different, checking with indexOf instead of === works
