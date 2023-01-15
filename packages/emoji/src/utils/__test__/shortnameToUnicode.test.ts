75a268dfd8735307c6de5a9b5e433bb8f958f3a4
Marco Burrometo <marco.burrometo@gmail.com>
2023-01-15 16:55:44 +0100

    tests

--- packages/emoji/src/utils/__test__/shortnameToUnicode.test.ts
+++ packages/emoji/src/utils/__test__/shortnameToUnicode.test.ts
@@ -1,15 +1,32 @@
+import { toShort } from 'emoji-toolkit';
+import data from 'emojibase-data/en/compact.json';
 import shortnameToUnicode from '../shortnameToUnicode';
 
 describe('shortnameToUnicode', () => {
-  test('empty shortname -> default', () => {
-    expect(shortnameToUnicode('')).toBe('');
-  });
-  test('valid shortname -> emoji', () => {
-    expect(shortnameToUnicode(':grinning:')).toBe('😀');
-    expect(shortnameToUnicode(':thumbsup:')).toBe('👍️');
+  it('should return the correct Unicode value for a given shortname', () => {
+    expect(shortnameToUnicode(':smile:')).toEqual('😄');
+    expect(shortnameToUnicode(':heart:')).toEqual('❤️');
   });
 
-  test('NOT valid shortname -> emoji', () => {
-    expect(shortnameToUnicode(':not_an_emoji:')).toBeUndefined();
+  it('should return an empty string for an invalid shortname', () => {
+    expect(shortnameToUnicode(':invalid:')).toEqual('');
+    expect(shortnameToUnicode(':not-a-shortname:')).toEqual('');
+  });
+
+  it('should return an empty string for an empty input', () => {
+    expect(shortnameToUnicode('')).toEqual('');
+  });
+
+  it('should return the correct Unicode value for all emojis', () => {
+    data.forEach((item) => {
+      const expectedUnicode = item.unicode;
+      const shortname = toShort(expectedUnicode);
+      expect(shortnameToUnicode(shortname)).toEqual(expectedUnicode);
+    });
+  });
+
+  // This test case checks if the function can correctly handle the :thumbsup: shortname which contains a utf-16 character and if it can find the correct matching Unicode value for that shortname.
+  it('should handle utf-16 strings correctly', () => {
+    expect(shortnameToUnicode(':thumbsup:')).toEqual('👍️');
   });
 });
