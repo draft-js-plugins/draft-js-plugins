b3eea5a629b6662516ae3618046cc3f7ddd2c206
TJ Taylor <tj@mobilize.io>
2023-08-02 23:47:42 -0600

    Shortcircuit looking up shortnames

--- packages/emoji/src/utils/createEmojisFromStrategy.ts
+++ packages/emoji/src/utils/createEmojisFromStrategy.ts
@@ -1,4 +1,4 @@
-import { emojiList, toShort } from 'emoji-toolkit';
+import { emojiList, toShort, shortnameLookup } from 'emoji-toolkit';
 import data from 'emojibase-data/en/compact.json';
 import { EmojiShape } from 'packages/emoji/src/constants/type';
 
@@ -6,6 +6,15 @@ export interface EmojiStrategy {
   [x: string]: {
     [x: string]: EmojiShape[];
   };
+}
+
+// `toShort` from the toolkit uses a long regular expression that can perform quite poorly.
+// We leverage their (incorrectly typed) `shortnameLookup` object to short-circuit the lookup for common Emoji.
+function fasterUnicodeToShortname(unicode: string): EmojiShape {
+  return (
+    (shortnameLookup as unknown as { [unicode: string]: string })[unicode] ||
+    toShort(unicode)
+  );
 }
 
 // Filtering out all non printable characters.
@@ -18,7 +27,7 @@ export default function createEmojisFromStrategy(): EmojiStrategy {
   const emojis: EmojiStrategy = {};
 
   for (const item of data) {
-    const shortName = toShort(item.unicode);
+    const shortName = fasterUnicodeToShortname(item.unicode);
     const emoji = emojiList[escapeNonASCIICharacters(shortName)];
     if (emoji) {
       if (!emojis[emoji.category]) {
@@ -28,7 +37,7 @@ export default function createEmojisFromStrategy(): EmojiStrategy {
 
       if (item.skins) {
         for (const skin of item.skins) {
-          const skinShortName = toShort(skin.unicode);
+          const skinShortName = fasterUnicodeToShortname(skin.unicode);
           if (emojiList[skinShortName]) {
             emojis[emoji.category][shortName].push(skinShortName);
           }
